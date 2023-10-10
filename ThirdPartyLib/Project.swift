11ecd9f2ff2f993c212d841ca2779729db6b0240
uuuunseo <jdbstjoo@gmail.com>
2023-10-10 10:30:04 +0900

    :recycle: :: dependency management style change

--- ThirdPartyLib/Project.swift
+++ ThirdPartyLib/Project.swift
@@ -3,9 +3,7 @@ import ProjectDescriptionHelpers
 
 let project = Project.dynamicFramwork(
     name: "ThirdPartyLib",
-    packages: [
-        .Swinject
-    ],
+    packages: [],
     deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone]),
     dependencies: [
         .SPM.Swinject
--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -3,8 +3,9 @@ import ProjectDescription
 let dependencies = Dependencies(
     carthage: nil,
     swiftPackageManager: SwiftPackageManagerDependencies(
-        [],
-        productTypes: [:],
+        [
+            .remote(url: "https://github.com/Swinject/Swinject.git", requirement: .exact("2.8.0"))
+        ],
         baseSettings: .settings(
             configurations: [
                 .debug(name: .debug),
