9c91f6d91517a161a0398d27571b4c2b094b92ed
uuuunseo <jdbstjoo@gmail.com>
2023-10-18 09:42:38 +0900

    :recycle: :: PrimaryColorSystem case 소문자로 수정

--- App/Sources/DesignSystem/Color/BitgouelColor+Primary.swift
+++ App/Sources/DesignSystem/Color/BitgouelColor+Primary.swift
@@ -3,30 +3,30 @@ import SwiftUI
 public extension Color.BitgouelColorSystem {
     // swiftlint:disable identifier_name
     enum Primary: BitgouelColorable {
-        case P1
-        case P2
-        case P3
-        case P4
-        case P5
-        case P6
-        case P7
-        case P8
-        case P9
+        case p1
+        case p2
+        case p3
+        case p4
+        case p5
+        case p6
+        case p7
+        case p8
+        case p9
     }
 }
 
 public extension Color.BitgouelColorSystem.Primary {
     var color: Color {
         switch self {
-        case .P1: return BitgouelAsset.Primary.p1.swiftUIColor
-        case .P2: return BitgouelAsset.Primary.p2.swiftUIColor
-        case .P3: return BitgouelAsset.Primary.p3.swiftUIColor
-        case .P4: return BitgouelAsset.Primary.p4.swiftUIColor
-        case .P5: return BitgouelAsset.Primary.p5.swiftUIColor
-        case .P6: return BitgouelAsset.Primary.p6.swiftUIColor
-        case .P7: return BitgouelAsset.Primary.p7.swiftUIColor
-        case .P8: return BitgouelAsset.Primary.p8.swiftUIColor
-        case .P9: return BitgouelAsset.Primary.p9.swiftUIColor
+        case .p1: return BitgouelAsset.Primary.p1.swiftUIColor
+        case .p2: return BitgouelAsset.Primary.p2.swiftUIColor
+        case .p3: return BitgouelAsset.Primary.p3.swiftUIColor
+        case .p4: return BitgouelAsset.Primary.p4.swiftUIColor
+        case .p5: return BitgouelAsset.Primary.p5.swiftUIColor
+        case .p6: return BitgouelAsset.Primary.p6.swiftUIColor
+        case .p7: return BitgouelAsset.Primary.p7.swiftUIColor
+        case .p8: return BitgouelAsset.Primary.p8.swiftUIColor
+        case .p9: return BitgouelAsset.Primary.p9.swiftUIColor
         }
     }
 }
