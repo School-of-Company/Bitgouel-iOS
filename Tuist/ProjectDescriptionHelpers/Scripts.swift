97dcdd69c791f71c4395d74f33a0cc9ea1306279
uuuunseo <jdbstjoo@gmail.com>
2023-10-18 17:09:08 +0900

    :hammer: :: 스크립트 수정

--- Tuist/ProjectDescriptionHelpers/Scripts.swift
+++ Tuist/ProjectDescriptionHelpers/Scripts.swift
@@ -3,6 +3,7 @@ import ProjectDescription
 public extension TargetScript {
     static let SwiftLint = TargetScript.pre(
         path: .relativeToRoot("Scripts/SwiftLintRunScript.sh"),
-        name: "SwiftLint"
+        name: "SwiftLint",
+        basedOnDependencyAnalysis: false
     )
 }
