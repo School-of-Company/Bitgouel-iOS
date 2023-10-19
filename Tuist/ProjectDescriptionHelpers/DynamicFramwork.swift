8d232ec8b82de3f42c859104f7449c0bda5d03dd
uuuunseo <jdbstjoo@gmail.com>
2023-10-19 09:43:12 +0900

    :hammer: :: scripts 추가

--- Tuist/ProjectDescriptionHelpers/DynamicFramwork.swift
+++ Tuist/ProjectDescriptionHelpers/DynamicFramwork.swift
@@ -6,6 +6,7 @@ extension Project{
         platform: Platform = .iOS,
         packages: [Package] = [],
         infoPlist: InfoPlist = .default,
+        scripts: [TargetScript] = generateEnvironment.scripts,
         deploymentTarget: DeploymentTarget,
         dependencies: [TargetDependency] = [
             .project(target: "ThirdPartyLib", path: Path("../ThirdPartyLib"))
