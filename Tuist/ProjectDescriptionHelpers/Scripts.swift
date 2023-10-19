5fa4f0ba128436fc93f5643806e118e82b5dc1cf
uuuunseo <jdbstjoo@gmail.com>
2023-10-19 19:37:42 +0900

    :green_heart: :: 파일 위치 변경

--- Tuist/ProjectDescriptionHelpers/Scripts.swift
+++ Tuist/ProjectDescriptionHelpers/Scripts.swift
@@ -8,7 +8,7 @@ if test -d "/opt/homebrew/bin/"; then
 fi
 
 export PATH
-YML="${PROJECT_DIR)/.swiftlint.yml"
+YML="$(dirname "$0")/.swiftlint.yml"
 
 if which swiftlint > /dev/vull; then
     swiftlint --config "${YML}"
