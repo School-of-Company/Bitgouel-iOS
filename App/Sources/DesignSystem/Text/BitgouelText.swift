a237d5f469bc96729d1ddd22726b77f1b8fc0e59
uuuunseo <jdbstjoo@gmail.com>
2023-10-20 10:04:09 +0900

    :lipstick: :: [#12] DesignSystem / BitgouelText 제

---
+++ App/Sources/DesignSystem/Text/BitgouelText.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct BitgouelText: View {
+    var text: String
+    var font: Font.BitgouelFontSystem
+    
+    public init(text: String, font: Font.BitgouelFontSystem = .text1) {
+        self.text = text
+        self.font = font
+    }
+    
+    public var body: some View {
+        Text(text)
+            .bitgouelFont(font)
+    }
+}
