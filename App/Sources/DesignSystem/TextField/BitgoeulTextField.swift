6d8011243c5d14a13726727ab10263f547669590
shwaaaa <shwaa1026@naver.com>
2023-10-26 23:06:13 +0900

    :twisted_rightwards_arrows: :: [#24] DesignSystem에 TextField 추가

---
+++ App/Sources/DesignSystem/TextField/BitgoeulTextField.swift
@@ -0,0 +1,84 @@
+//
+//  BitgoeulTextField.swift
+//  Bitgouel
+//
+//  Created by 이승화 on 10/23/23.
+//  Copyright © 2023 team.msg. All rights reserved.
+//
+
+import SwiftUI
+
+public struct BitgoeulTextField: View {
+    var placeholder: String
+    var helpMessage: String
+    var isError: Bool
+    var isEmpty: Bool
+    var onSubmit: () -> Void
+    @Binding var text: String
+    @FocusState var isFocused: Bool
+    @Environment(\.isEnabled) var isEnabled
+    
+    private var borderForegroundColor: Color {
+        isFocused ?
+            Color.bitgouel(.primary(.p5)):
+        isError ?
+            Color.bitgouel(.error(.e5)):
+            Color.bitgouel(.greyscale(.g7))
+        }
+    
+    private var inputForegroundColor: Color {
+        isError ?
+            Color.bitgouel(.error(.e5)):
+        isFocused || isEmpty ?
+            Color.black:
+            Color.bitgouel(.greyscale(.g7))
+    }
+    
+    public init(
+        _ placeholder: String = "",
+        text: Binding<String>,
+        helpMessage: String = "",
+        isError: Bool = false,
+        isEmpty: Bool = false,
+        onSubmit: @escaping () -> Void = {}
+    ) {
+        self.placeholder = placeholder
+        self._text = text
+        self.helpMessage = helpMessage
+        self.isError = isError
+        self.onSubmit = onSubmit
+        self.isEmpty = isEmpty
+    }
+    
+    public var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            HStack {
+                TextField(placeholder, text: $text)
+                    .onSubmit(onSubmit)
+                    .focused($isFocused)
+                    .bitgouelFont(.text3)
+                    .foregroundColor(inputForegroundColor)
+                    .background(isEnabled ? Color.white : Color.bitgouel(.greyscale(.g7)))
+                    .disabled(isEnabled ? false : true)
+                Button{
+                    text = ""
+                } label: {
+                    BitgouelAsset.Assets.cancel.swiftUIImage
+                }
+            }
+            .frame(height: 54)
+            .padding(.horizontal,20)
+            .cornerRadius(8)
+            .overlay{
+                RoundedRectangle(cornerRadius: 8)
+                    .strokeBorder(borderForegroundColor)
+            }
+            .onTapGesture {
+                isFocused = true
+            }
+            
+            Text(helpMessage)
+                .bitgouelFont(.text3, color: isError ? .error(.e5) : .greyscale(.g4))
+        }
+    }
+}
---
+++ App/Sources/DesignSystem/TextField/SecureBitgoeulTextField.swift
@@ -0,0 +1,94 @@
+//
+//  SecureBitgoeulTextField.swift
+//  Bitgouel
+//
+//  Created by 이승화 on 10/25/23.
+//  Copyright © 2023 team.msg. All rights reserved.
+//
+
+import SwiftUI
+
+public struct SecureBitgoeulTextField: View {
+    var placeholder: String
+    var helpMessage: String
+    var isError: Bool
+    var isEmpty: Bool
+    @State var isSecure = true
+    var onSubmit: () -> Void
+    @Binding var text: String
+    @FocusState var isFocused: Bool
+    @Environment(\.isEnabled) var isEnabled
+
+    private var borderForegroundColor: Color {
+        isFocused ?
+            Color.bitgouel(.primary(.p5)):
+        isError ?
+            Color.bitgouel(.error(.e5)):
+            Color.bitgouel(.greyscale(.g7))
+        }
+    
+    private var inputForegroundColor: Color {
+        isError ?
+            Color.bitgouel(.error(.e5)):
+        isFocused || isEmpty ?
+            Color.black:
+            Color.bitgouel(.greyscale(.g7))
+    }
+    
+    public init(
+        _ placeholder: String = "",
+        text: Binding<String>,
+        helpMessage: String = "",
+        isError: Bool = false,
+        isEmpty: Bool = false,
+        onSubmit: @escaping () -> Void = {}
+    ) {
+        self.placeholder = placeholder
+        self._text = text
+        self.helpMessage = helpMessage
+        self.isError = isError
+        self.onSubmit = onSubmit
+        self.isEmpty = isEmpty
+    }
+    
+    public var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            HStack {
+                Group {
+                    if isSecure {
+                        SecureField(placeholder, text: $text)
+                    } else {
+                        TextField(placeholder, text: $text)
+                    }
+                }
+                .onSubmit(onSubmit)
+                .focused($isFocused)
+                .bitgouelFont(.text3)
+                .foregroundColor(inputForegroundColor)
+                .background(isEnabled ? Color.white : Color.bitgouel(.greyscale(.g7)))
+                .disabled(isEnabled ? false : true)
+                
+                Button{
+                    isSecure.toggle()
+                } label: {
+                    isSecure ?
+                        BitgouelAsset.Assets.eye.swiftUIImage :
+                        BitgouelAsset.Assets.eyeSlash.swiftUIImage
+                }
+            }
+            .frame(height: 54)
+            .padding(.horizontal,20)
+            .cornerRadius(8)
+            .overlay{
+                RoundedRectangle(cornerRadius: 8)
+                    .strokeBorder(borderForegroundColor)
+            }
+            .onTapGesture {
+                isFocused = true
+            }
+            
+            Text(helpMessage)
+                .bitgouelFont(.text3, color: isError ? .error(.e5) : .greyscale(.g4))
+        }
+    }
+}
