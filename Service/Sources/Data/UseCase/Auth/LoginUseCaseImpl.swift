388a30ca0edd9ca15645a0fadf22676fccb37f2e
uuuunseo <jdbstjoo@gmail.com>
2023-10-30 12:26:06 +0900

    :recycle: :: [#34] AuthDomain / SwiftFormat

--- Service/Sources/Data/UseCase/Auth/LoginUseCaseImpl.swift
+++ Service/Sources/Data/UseCase/Auth/LoginUseCaseImpl.swift
@@ -2,11 +2,11 @@ import Foundation
 
 public struct LoginUseCaseImpl: LoginUseCase {
     private let authRepository: any AuthRepository
-    
+
     public init(authRepository: any AuthRepository) {
         self.authRepository = authRepository
     }
-    
+
     public func execute(req: LoginRequestDTO) async throws {
         try await authRepository.login(req: req)
     }
