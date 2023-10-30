9c43a7799aa95d43e68a2af42718ab67c0a5965c
uuuunseo <jdbstjoo@gmail.com>
2023-10-30 13:59:46 +0900

    :sparkles: :: [#36] AuthDomain / reissueToken DataSource

--- Service/Sources/Domain/AuthDomain/DataSource/RemoteAuthDataSource.swift
+++ Service/Sources/Domain/AuthDomain/DataSource/RemoteAuthDataSource.swift
@@ -1,6 +1,11 @@
 import Foundation
 
 final class RemoteAuthDataSource: BaseRemoteDataSource<AuthAPI> {
+    
+    func reissueToken() async throws {
+        try await request(.reissueToken)
+    }
+    
     func studentSignup(req: StudentSignupRequestDTO) async throws {
         try await request(.studentSignup(req))
     }
