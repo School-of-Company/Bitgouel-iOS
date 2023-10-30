e6545d337db56cfa553addaf5cfd4beea55b281f
uuuunseo <jdbstjoo@gmail.com>
2023-10-30 11:55:53 +0900

    :recycle: :: [#21] AuthDomain / SwiftFormat

--- Service/Sources/Data/UseCase/Auth/StudentSignupUseCaseImpl.swift
+++ Service/Sources/Data/UseCase/Auth/StudentSignupUseCaseImpl.swift
@@ -2,11 +2,11 @@ import Foundation
 
 struct StudentSignupUseCaseImpl: StudentSignupUseCase {
     private let authRepository: any AuthRepository
-    
+
     init(authRepository: any AuthRepository) {
         self.authRepository = authRepository
     }
-    
+
     func execute(req: StudentSignupRequestDTO) async throws {
         try await authRepository.studentSignup(req: req)
     }
