5c5e83928f98fa206ca218de4670e94452c53748
uuuunseo <jdbstjoo@gmail.com>
2023-10-30 17:57:11 +0900

    :sparkles: :: [#40] RemoteAuthDataSource / 회원탈퇴 추가

--- Service/Sources/Domain/AuthDomain/DataSource/RemoteAuthDataSource.swift
+++ Service/Sources/Domain/AuthDomain/DataSource/RemoteAuthDataSource.swift
@@ -13,6 +13,10 @@ final class RemoteAuthDataSource: BaseRemoteDataSource<AuthAPI> {
     func logout() async throws {
         try await request(.logout)
     }
+    
+    func withdraw() async throws {
+        try await request(.withdraw)
+    }
 
     func studentSignup(req: StudentSignupRequestDTO) async throws {
         try await request(.studentSignup(req))
