905f1d25cdad37670f7dddd3b3c3b88d9ba4d5eb
uuuunseo <jdbstjoo@gmail.com>
2023-10-30 14:09:38 +0900

    :recycle: :: [#36] AuthDomain / SwiftFormat

--- Service/Sources/Data/UseCase/Auth/ReissueTokenUseCaseImpl.swift
+++ Service/Sources/Data/UseCase/Auth/ReissueTokenUseCaseImpl.swift
@@ -2,11 +2,11 @@ import Foundation
 
 struct ReissueTokenUseCaseImpl: ReissueTokenUseCase {
     private let authRepository: any AuthRepository
-    
+
     init(authRepository: any AuthRepository) {
         self.authRepository = authRepository
     }
-    
+
     func execute() async throws {
         try await authRepository.reissueToken()
     }
