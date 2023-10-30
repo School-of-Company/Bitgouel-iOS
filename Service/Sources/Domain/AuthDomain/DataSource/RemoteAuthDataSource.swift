7cc46527b84c8f6184b055bcb58af9bb12ad3153
uuuunseo <jdbstjoo@gmail.com>
2023-10-30 14:52:31 +0900

    :sparkles: :: [#38] AuthDomain / RemoteAuthDataSource

--- Service/Sources/Domain/AuthDomain/DataSource/RemoteAuthDataSource.swift
+++ Service/Sources/Domain/AuthDomain/DataSource/RemoteAuthDataSource.swift
@@ -1,6 +1,11 @@
 import Foundation
 
 final class RemoteAuthDataSource: BaseRemoteDataSource<AuthAPI> {
+    
+    func logout() async throws {
+        try await request(.logout)
+    }
+    
     func studentSignup(req: StudentSignupRequestDTO) async throws {
         try await request(.studentSignup(req))
     }
