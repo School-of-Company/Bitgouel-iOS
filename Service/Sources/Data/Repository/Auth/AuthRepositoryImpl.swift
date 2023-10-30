f344a2adf1395a1a98c2612d588c6f5372953bc7
uuuunseo <jdbstjoo@gmail.com>
2023-10-30 18:00:25 +0900

    :sparkles: :: [#40] AuthRepository / withdraw 함수 추가

--- Service/Sources/Data/Repository/Auth/AuthRepositoryImpl.swift
+++ Service/Sources/Data/Repository/Auth/AuthRepositoryImpl.swift
@@ -12,13 +12,17 @@ struct AuthRepositoryImpl: AuthRepository {
     public func login(req: LoginRequestDTO) async throws {
         try await remoteAuthDataSource.login(req: req)
     }
-  
+
     public func reissueToken() async throws {
         try await remoteAuthDataSource.reissueToken()
     }
 
     public func logout() async throws {
         try await remoteAuthDataSource.logout()
+    }
+    
+    public func withdraw() async throws {
+        try await remoteAuthDataSource.withdraw()
     }
 
     public func studentSignup(req: StudentSignupRequestDTO) async throws {
--- Service/Sources/Domain/AuthDomain/DataSource/RemoteAuthDataSource.swift
+++ Service/Sources/Domain/AuthDomain/DataSource/RemoteAuthDataSource.swift
@@ -1,19 +1,18 @@
 import Foundation
 
 final class RemoteAuthDataSource: BaseRemoteDataSource<AuthAPI> {
-  
     func login(req: LoginRequestDTO) async throws {
         try await request(.login(req))
     }
-  
+
     func reissueToken() async throws {
         try await request(.reissueToken)
     }
-    
+
     func logout() async throws {
         try await request(.logout)
     }
-    
+
     func withdraw() async throws {
         try await request(.withdraw)
     }
