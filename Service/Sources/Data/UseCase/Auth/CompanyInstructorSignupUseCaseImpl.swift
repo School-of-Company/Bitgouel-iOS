64bd526f7bf4ee3d69bdbe84889ef9e6b1c241b6
uuuunseo <jdbstjoo@gmail.com>
2023-10-31 11:29:34 +0900

    :recycle: :: [#51] AuthDomain / swiftformat

--- Service/Sources/Data/UseCase/Auth/CompanyInstructorSignupUseCaseImpl.swift
+++ Service/Sources/Data/UseCase/Auth/CompanyInstructorSignupUseCaseImpl.swift
@@ -2,11 +2,11 @@ import Foundation
 
 struct CompanyInstructorSignupUseCaseImpl: CompanyInstructorSignupUseCase {
     private let authRepository: any AuthRepository
-    
-    init(authRepository: any AuthRepository){
+
+    init(authRepository: any AuthRepository) {
         self.authRepository = authRepository
     }
-    
+
     func callAsFunction(req: CompanyInstructorSignupRequestDTO) async throws {
         try await authRepository.companyInstructorSignup(req: req)
     }
