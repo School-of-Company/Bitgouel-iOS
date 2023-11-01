96ab30bcfc634f9a9c93b7d64dba4bef355e2575
uuuunseo <jdbstjoo@gmail.com>
2023-11-01 18:31:52 +0900

    :sparkles: :: [#55] LectureDomain / Repository 추가

--- Service/Sources/Data/Repository/Lecture/LectureRepositoryImpl.swift
+++ Service/Sources/Data/Repository/Lecture/LectureRepositoryImpl.swift
@@ -1,8 +1,9 @@
-//
-//  LectureRepositoryImpl.swift
-//  Service
-//
-//  Created by 정윤서 on 2023/11/01.
-//
-
 import Foundation
+
+struct LectureRepositoryImpl: LectureRepository {
+    private let remoteLectureDataSource: RemoteLectureDataSource
+
+    init(remoteLectureDataSource: RemoteLectureDataSource) {
+        self.remoteLectureDataSource = remoteLectureDataSource
+    }
+}
