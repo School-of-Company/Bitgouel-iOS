27867bcaa36d46bd64c2aa8d5d1c7f9e21deb1f3
uuuunseo <jdbstjoo@gmail.com>
2023-11-01 23:38:38 +0900

    :sparkles: :: [#57] LectureRepository / lectureOpen 추가

--- App/Sources/DesignSystem/Button/BitgouelButton.swift
+++ App/Sources/DesignSystem/Button/BitgouelButton.swift
@@ -4,30 +4,29 @@ public struct BitgouelButton: View {
     var text: String
     var style: ButtonStyleType
     var action: () -> Void
-    
+
     public init(
         text: String,
         style: ButtonStyleType = .primary,
         action: @escaping () -> Void = {}
-        
+
     ) {
         self.text = text
         self.style = .primary
         self.action = action
     }
-    
+
     public var body: some View {
         HStack {
             Spacer()
-            
+
             Text(text)
                 .padding(.horizontal, 32)
                 .padding(.vertical, 13.5)
-            
+
             Spacer()
         }
         .buttonWrapper(action)
         .buttonStyle(BitgouelButtonStyle(style: style))
     }
 }
-
