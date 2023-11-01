fb6804d9df7eb104d86d6853b02ea39502985c4f
uuuunseo <jdbstjoo@gmail.com>
2023-11-01 18:24:49 +0900

    :sparkles: :: [#55] LectureDomain / LectureAPI 추가

--- Service/Sources/Domain/LectureDomain/API/LectureAPI.swift
+++ Service/Sources/Domain/LectureDomain/API/LectureAPI.swift
@@ -1,8 +1,54 @@
-//
-//  LectureAPI.swift
-//  Service
-//
-//  Created by 정윤서 on 2023/11/01.
-//
-
 import Foundation
+import Moya
+
+public enum LectureAPI {
+    case lectureOpen(LectureOpenRequestDTO)
+}
+
+extension LectureAPI: BitgouelAPI {
+    public typealias ErrorType = LectureDomainError
+
+    public var domain: BitgouelDomain {
+        .lecture
+    }
+
+    public var urlPath: String {
+        switch self {
+        case .lectureOpen:
+            return ""
+        }
+    }
+
+    public var method: Moya.Method {
+        switch self {
+        case .lectureOpen:
+            return .post
+        }
+    }
+
+    public var task: Moya.Task {
+        switch self {
+        case let .lectureOpen(req):
+            return .requestJSONEncodable(req)
+        }
+    }
+
+    public var jwtTokenType: JwtTokenType {
+        switch self {
+        case .lectureOpen(_):
+            return .accessToken
+        }
+    }
+
+    public var errorMap: [Int : LectureDomainError] {
+        switch self {
+        case .lectureOpen(_):
+            return [
+                400: .badRequest,
+                401: .unauthorized,
+                403: .forbidden,
+                409: .conflict
+            ]
+        }
+    }
+}
