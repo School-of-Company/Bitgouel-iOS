fcfdf918bc3c158158de3824d213ccae7bf72fe2
uuuunseo <jdbstjoo@gmail.com>
2023-11-03 11:26:00 +0900

    :sparkles: :: [#57] RemoteLectureDataSource / lectureApply 추가

--- Service/Sources/Domain/LectureDomain/DataSource/RemoteLectureDataSource.swift
+++ Service/Sources/Domain/LectureDomain/DataSource/RemoteLectureDataSource.swift
@@ -4,4 +4,8 @@ final class RemoteLectureDataSource: BaseRemoteDataSource<LectureAPI> {
     func lectureOpen(req: LectureOpenRequestDTO) async throws {
         try await request(.lectureOpen(req))
     }
+
+    func lectureApply(userID: String) async throws {
+        try await request(.lectureApply(userID: userID))
+    }
 }
--- Service/Sources/Domain/LectureDomain/Error/LectureDomainError.swift
+++ Service/Sources/Domain/LectureDomain/Error/LectureDomainError.swift
@@ -20,13 +20,13 @@ extension LectureDomainError: LocalizedError {
 
         case .forbidden:
             return "권한이 없습니다."
-            
+
         case .notFound:
             return "대상을 찾을 수 없습니다."
 
         case .conflict:
             return "신청기간이 유효하지 않습니다."
-            
+
         case .tooManyRequest:
             return "요청이 너무 많습니다."
         }
