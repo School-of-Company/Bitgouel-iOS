15ce611e5db94f6f3245dbda9099c147c2bf35d1
uuuunseo <jdbstjoo@gmail.com>
2023-11-03 14:30:28 +0900

    :sparkles: :: [#57] LectureUseCase / WaitingUseCase추가

--- Service/Sources/Data/Repository/Lecture/LectureRepositoryImpl.swift
+++ Service/Sources/Data/Repository/Lecture/LectureRepositoryImpl.swift
@@ -14,7 +14,7 @@ struct LectureRepositoryImpl: LectureRepository {
     public func lectureApply(userID: String) async throws {
         try await remoteLectureDataSource.lectureApply(userID: userID)
     }
-    
+
     public func waitingLectureApprove(userID: String) async throws {
         try await remoteLectureDataSource.waitingLectureApprove(userID: userID)
     }
