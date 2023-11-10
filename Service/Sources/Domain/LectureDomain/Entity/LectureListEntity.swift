871053da2af76798fc7b1fe7a88a3b67f51daad0
uuuunseo <jdbstjoo@gmail.com>
2023-11-10 15:40:16 +0900

    :sparkles: :: [#73] LectureEntity / add LectureListEntity

--- Service/Sources/Domain/LectureDomain/Entity/LectureListEntity.swift
+++ Service/Sources/Domain/LectureDomain/Entity/LectureListEntity.swift
@@ -1,8 +1,41 @@
-//
-//  LectureListEntity.swift
-//  Service
-//
-//  Created by 정윤서 on 2023/11/10.
-//
-
 import Foundation
+
+public struct LectureListEntity: Equatable {
+    public var id = UUID()
+    public let name: String
+    public let content: String
+    public let startDate: String
+    public let endDate: String
+    public let completeDate: String
+    public let lectureType: LectureType
+    public let lectureStatus: LectureStatusType
+    public let headCount: Int
+    public let maxRegisteredUser: Int
+    public let lecturer: String
+    
+    public init(
+        id: UUID,
+        name: String,
+        content: String,
+        startDate: String,
+        endDate: String,
+        completeDate: String,
+        lectureType: LectureType,
+        lectureStatus: LectureStatusType,
+        headCount: Int,
+        maxRegisteredUser: Int,
+        lecturer: String
+    ) {
+        self.id = id
+        self.name = name
+        self.content = content
+        self.startDate = startDate
+        self.endDate = endDate
+        self.completeDate = completeDate
+        self.lectureType = lectureType
+        self.lectureStatus = lectureStatus
+        self.headCount = headCount
+        self.maxRegisteredUser = maxRegisteredUser
+        self.lecturer = lecturer
+    }
+}
