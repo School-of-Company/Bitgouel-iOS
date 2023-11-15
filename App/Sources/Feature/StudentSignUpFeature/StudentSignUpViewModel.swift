f0074fa32bf4286f26b974277a94428f243e7837
이승화 <127721819+shwaaaa@users.noreply.github.com>
2023-11-15 22:26:47 +0900

    Update App/Sources/Feature/StudentSignUpFeature/StudentSignUpViewModel.swift
    Co-authored-by: baegteun <baegteun@gmail.com>

--- App/Sources/Feature/StudentSignUpFeature/StudentSignUpViewModel.swift
+++ App/Sources/Feature/StudentSignUpFeature/StudentSignUpViewModel.swift
@@ -21,7 +21,7 @@ final class StudentSignUpViewModel: ObservableObject {
     @Published var certificationNumberEmail = ""
     @Published var password = ""
     @Published var checkPassword = ""
-    @Published var phoneNumberTimeRemaining : Int = 180
+    @Published var phoneNumberTimeRemaining: Int = 180
     @Published var emailTimeRemaining : Int = 180
     @Published var selectedSchool: String = "학교"
     @Published var selectedClub: String = "동아리"
