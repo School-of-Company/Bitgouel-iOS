c9738b273a94ccbba256225e06ab48bf7386d12f
kimkyumbi <kimgyeombi@naver.com>
2023-11-16 14:23:01 +0900

    ♻️ :: UserRoleType enum에 함수 추가

--- Service/Sources/Base/Enum/UserRoleType.swift
+++ Service/Sources/Base/Enum/UserRoleType.swift
@@ -13,3 +13,23 @@ public enum UserRoleType {
         case grovernment = "유관 기관"
     }
 }
+
+public extension UserRoleType.SchoolType {
+    func schoolValue() -> String {
+        switch self {
+        case .student: return "학생"
+        case .teacher: return "취업동아리 선생님"
+        }
+    }
+}
+
+public extension UserRoleType.ExternalType {
+    func externalValue() -> String {
+        switch self {
+        case .companyInstructor: return "기업 강사"
+        case .professor: return "대학 교수"
+        case .bbozzack: return "뽀짝 선생님"
+        case .grovernment: return "유관 기관"
+        }
+    }
+}
