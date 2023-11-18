d730667863e8d97d345c3ba13013f1628044a716
kimkyumbi <kimgyeombi@naver.com>
2023-11-18 23:37:12 +0900

    🔀 :: [#84] Extension의 ButtonWrapper에 cornerRadius추가

---
+++ App/Sources/Feature/LectureOpeningApplyFeature/LectureOpeningApplyView.swift
@@ -0,0 +1,19 @@
+//
+//  LectureOpeningApplyView.swift
+//  Bitgouel
+//
+//  Created by Mac on 11/17/23.
+//  Copyright © 2023 team.msg. All rights reserved.
+//
+
+import SwiftUI
+
+struct LectureOpeningApplyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LectureOpeningApplyView()
+}
---
+++ App/Sources/Feature/LectureOpeningApplyFeature/LectureOpeningApplyViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  LectureOpeningApplyView.swift
+//  Bitgouel
+//
+//  Created by Mac on 11/17/23.
+//  Copyright © 2023 team.msg. All rights reserved.
+//
+
+import Foundation
