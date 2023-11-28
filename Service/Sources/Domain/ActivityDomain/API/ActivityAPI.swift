1f760551d6ff7f0a46887c8c7107b5b8a36d86a1
uuuunseo <jdbstjoo@gmail.com>
2023-11-28 18:16:55 +0900

    :sparkles: :: [#93] ActivityDomain / Activity 세팅

---
+++ Service/Sources/Domain/ActivityDomain/API/ActivityAPI.swift
@@ -0,0 +1,38 @@
+import Foundation
+import Moya
+
+public enum ActivityDomain {
+}
+
+extension ActivityAPI: BitgouelAPI {
+    public var domain: BitgouelDomain {
+        .activity
+    }
+    
+    public var urlPath: String {
+        switch self {
+        }
+    }
+    
+    public var method: Moya.Method {
+        switch self {
+        }
+    }
+    
+    public var task: Moya.Task {
+        switch self {
+        }
+    }
+    
+    public var jwtTokenType: JwtTokenType {
+        switch self {
+            
+        }
+    }
+    
+    public var errorMap: [Int: ErrorType] {
+        switch self {
+            
+        }
+    }
+}
