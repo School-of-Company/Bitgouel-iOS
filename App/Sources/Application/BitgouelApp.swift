d199f2ae7b76347017267b731f5ca43c10057257
uuuunseo <jdbstjoo@gmail.com>
2023-11-28 14:09:24 +0900

    :fire: :: [#75] BitgouelApp / Keychain 삭제

--- App/Sources/Application/BitgouelApp.swift
+++ App/Sources/Application/BitgouelApp.swift
@@ -4,11 +4,9 @@ import Service
 @main
 struct BitgouelApp: App {
     @StateObject var appState = SceneState(sceneFlow: .login)
-    private let keychain: any Keychain
     
     init() {
         registerProviderFactories()
-        self.keychain = KeychainImpl()
     }
     
     var body: some Scene {
