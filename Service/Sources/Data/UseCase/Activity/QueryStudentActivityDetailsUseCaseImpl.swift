037b84d9586a5a85e1955afa8f7653e7f8a79aa0
uuuunseo <jdbstjoo@gmail.com>
2023-12-01 09:07:29 +0900

    :sparkles: :: [#93] ResponseDTO / StudentActivityDetailResponseDTO 추가

---
+++ Service/Sources/Data/UseCase/Activity/QueryStudentActivityDetailsUseCaseImpl.swift
@@ -0,0 +1,8 @@
+//
+//  QueryStudentActivityDetailsUseCaseImpl.swift
+//  Service
+//
+//  Created by 정윤서 on 12/1/23.
+//
+
+import Foundation
---
+++ Service/Sources/Domain/ActivityDomain/UseCase/QueryStudentActivityDetailsUseCase.swift
@@ -0,0 +1,8 @@
+//
+//  QueryStudentActivityDetailsUseCase.swift
+//  Service
+//
+//  Created by 정윤서 on 12/1/23.
+//
+
+import Foundation
