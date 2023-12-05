62e94d2c2931c3c53ca5cde8a73150a8b3dd9cc3
uuuunseo <jdbstjoo@gmail.com>
2023-12-06 03:55:10 +0900

    :recycle: :: [#97] StudentSignUpView / 선택사항별로 함수로 나누기

--- App/Sources/Feature/StudentSignUpFeature/StudentSignUpView.swift
+++ App/Sources/Feature/StudentSignUpFeature/StudentSignUpView.swift
@@ -14,6 +14,7 @@ struct StudentSignUpView: View {
     }
     @FocusState private var focusField: FocusField?
     @StateObject var viewModel: StudentSignUpViewModel
+    @StateObject var commonViewModel: CommonSignUpViewModel
     @State var isSchool = false
     @State var isShowingClubSelectSheet = false
     @State var isShowingSuccessView = false
@@ -49,19 +50,6 @@ struct StudentSignUpView: View {
                 SchoolListView(viewModel: viewModel)
                     .frame(height: 415)
             }
-            .bitgouelBottomSheet(
-                isShowing: $isShowingClubSelectSheet
-            ) {
-                ClubListView(
-                    searchText: $viewModel.clubSearch,
-                    searchedClubList: viewModel.searchedClubList,
-                    selectedClub: viewModel.selectedClub,
-                    clubDidSelect: { selectedClub in
-                        viewModel.selectedClub = selectedClub
-                    }
-                )
-                .frame(height: 415)
-            }
             .background(
                 NavigationLink(
                     destination: SuccessSignUpView(),
@@ -76,136 +64,6 @@ struct StudentSignUpView: View {
     @ViewBuilder
     func enterInformation() -> some View {
         VStack(spacing: 16) {
-            Group {
-                if !viewModel.password.isEmpty {
-                    SecureBitgouelTextField(
-                        "비밀번호",
-                        text: $viewModel.checkPassword
-                    )
-                    .focused($focusField, equals: .checkPassword)
-                    .padding(.bottom, -20)
-                    .onSubmit {
-                        if viewModel.isPasswordMatching {
-                            isShowingSuccessView = true
-                        } else {
-                            isShowingSuccessView = false
-                        }
-                    }
-                }
-                
-                if !viewModel.certificationNumberEmail.isEmpty {
-                    SecureBitgouelTextField(
-                        "비밀번호",
-                        text: $viewModel.password,
-                        onSubmit: {
-                            focusField = .checkPassword
-                        }
-                    )
-                    .focused($focusField, equals: .password)
-                    .padding(.bottom, -20)
-                }
-            }
-            
-            Group {
-                if !viewModel.email.isEmpty {
-                    BitgouelTextField(
-                        "인증번호",
-                        text: $viewModel.certificationNumberEmail,
-                        helpMessage: viewModel.convertSecondsToTime(timeInSeconds: viewModel.emailTimeRemaining),
-                        link: "재발송",
-                        onSubmit: {
-                            focusField = .password
-                        }
-                    )
-                    .focused($focusField, equals: .certificationEmail)
-                }
-                
-                if !viewModel.certificationNumberPhoneNumber.isEmpty {
-                    BitgouelTextField(
-                        "이메일",
-                        text: $viewModel.email,
-                        helpMessage: viewModel.emailHelpMessage,
-                        isError: viewModel.isEmailErrorOccured,
-                        onSubmit: {
-                            focusField = .certificationEmail
-                        }
-                    )
-                    .onChange(of: viewModel.emailHelpMessage) { newValue in }
-                    .focused($focusField, equals: .email)
-                    .textContentType(.emailAddress)
-                }
-            }
-            
-            Group {
-                if !viewModel.phoneNumber.isEmpty {
-                    BitgouelTextField(
-                        "인증번호",
-                        text: $viewModel.certificationNumberPhoneNumber,
-                        helpMessage: viewModel.convertSecondsToTime(timeInSeconds: viewModel.phoneNumberTimeRemaining),
-                        link: "재발송",
-                        onSubmit: {
-                            focusField = .email
-                        }
-                    )
-                    .focused($focusField, equals: .certificationNumberPhoneNumber)
-                }
-                
-                if !viewModel.studentID.isEmpty {
-                    BitgouelTextField(
-                        "전화번호",
-                        text: $viewModel.phoneNumber,
-                        onSubmit: {
-                            focusField = .certificationNumberPhoneNumber
-                        }
-                    )
-                    .onChange(of: viewModel.phoneNumber) { newValue in
-                        if !viewModel.phoneNumber.isEmpty {
-                            viewModel.phoneNumberStartTimer()
-                        }
-                    }
-                    .focused($focusField, equals: .phoneNumber)
-                    .padding(.bottom, -20)
-                }
-            }
-            
-            Group {
-                if !viewModel.yearOfAdmission.isEmpty {
-                    BitgouelTextField(
-                        "학번",
-                        text: $viewModel.studentID,
-                        onSubmit: {
-                            focusField = .phoneNumber
-                        }
-                    )
-                    .focused($focusField, equals: .studentID)
-                    .padding(.bottom, -20)
-                }
-                
-                if !viewModel.name.isEmpty {
-                    BitgouelTextField(
-                        "입학년도",
-                        text: $viewModel.yearOfAdmission,
-                        onSubmit: {
-                            focusField = .studentID
-                        }
-                    )
-                    .focused($focusField, equals: .yearOfAdmission)
-                    .padding(.bottom, -20)
-                }
-                
-                if viewModel.selectedClubExists {
-                    BitgouelTextField(
-                        "이름",
-                        text: $viewModel.name,
-                        onSubmit: {
-                            focusField = .yearOfAdmission
-                        }
-                    )
-                    .focused($focusField, equals: .name)
-                    .padding(.bottom, -20)
-                }
-            }
-            
             if viewModel.selectedSchoolExists {
                 AssociationSelectButton(
                     text: viewModel.selectedClub
@@ -220,9 +78,177 @@ struct StudentSignUpView: View {
                 isSchool.toggle()
             }
             
-            AssociationSelectButton(text: "학생")
-            
             AssociationSelectButton(text: "학교")
+
+            AssociationSelectButton(text: "학생")
+        }
+    }
+    
+    @ViewBuilder
+    func inputEmail() -> some View {
+        VStack(spacing: 16) {
+            if !viewModel.email.isEmpty {
+                BitgouelTextField(
+                    "인증번호",
+                    text: $viewModel.certificationNumberEmail,
+                    helpMessage: viewModel.convertSecondsToTime(timeInSeconds: viewModel.emailTimeRemaining),
+                    link: "재발송",
+                    onSubmit: {
+                        focusField = .password
+                    }
+                )
+                .focused($focusField, equals: .certificationEmail)
+            }
+            
+            if !viewModel.certificationNumberPhoneNumber.isEmpty {
+                BitgouelTextField(
+                    "이메일",
+                    text: $viewModel.email,
+                    helpMessage: viewModel.emailHelpMessage,
+                    isError: viewModel.isEmailErrorOccured,
+                    onSubmit: {
+                        focusField = .certificationEmail
+                    }
+                )
+                .onChange(of: viewModel.emailHelpMessage) { newValue in }
+                .focused($focusField, equals: .email)
+                .textContentType(.emailAddress)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func inputName() -> some View {
+        VStack(spacing: 16) {
+            if viewModel.selectedClubExists {
+                BitgouelTextField(
+                    "이름",
+                    text: $viewModel.name,
+                    onSubmit: {
+                        focusField = .yearOfAdmission
+                    }
+                )
+                .focused($focusField, equals: .name)
+                .padding(.bottom, -20)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func inputPhoneNumberView() -> some View {
+        VStack(spacing: 16) {
+            if !viewModel.phoneNumber.isEmpty {
+                BitgouelTextField(
+                    "인증번호",
+                    text: $viewModel.certificationNumberPhoneNumber,
+                    helpMessage: viewModel.convertSecondsToTime(timeInSeconds: viewModel.phoneNumberTimeRemaining),
+                    link: "재발송",
+                    onSubmit: {
+                        focusField = .email
+                    }
+                )
+                .focused($focusField, equals: .certificationNumberPhoneNumber)
+            }
+            
+            if !viewModel.studentID.isEmpty {
+                BitgouelTextField(
+                    "전화번호",
+                    text: $viewModel.phoneNumber,
+                    onSubmit: {
+                        focusField = .certificationNumberPhoneNumber
+                    }
+                )
+                .onChange(of: viewModel.phoneNumber) { newValue in
+                    if !viewModel.phoneNumber.isEmpty {
+                        viewModel.phoneNumberStartTimer()
+                    }
+                }
+                .focused($focusField, equals: .phoneNumber)
+                .padding(.bottom, -20)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func inputPassword() -> some View {
+        VStack(spacing: 16){
+            if !viewModel.password.isEmpty {
+                SecureBitgouelTextField(
+                    "비밀번호",
+                    text: $viewModel.checkPassword
+                )
+                .focused($focusField, equals: .checkPassword)
+                .padding(.bottom, -20)
+                .onSubmit {
+                    if viewModel.isPasswordMatching {
+                        isShowingSuccessView = true
+                    } else {
+                        isShowingSuccessView = false
+                    }
+                }
+            }
+            
+            if !viewModel.certificationNumberEmail.isEmpty {
+                SecureBitgouelTextField(
+                    "비밀번호",
+                    text: $viewModel.password,
+                    onSubmit: {
+                        focusField = .checkPassword
+                    }
+                )
+                .focused($focusField, equals: .password)
+                .padding(.bottom, -20)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func inputClubInfoView() -> some View {
+        VStack(spacing: 16) {
+            ScrollView {}
+                .bitgouelBottomSheet(
+                    isShowing: $isShowingClubSelectSheet
+                ) {
+                    ClubListView(
+                        searchText: $viewModel.clubSearch,
+                        searchedClubList: viewModel.searchedClubList,
+                        selectedClub: viewModel.selectedClub,
+                        clubDidSelect: { selectedClub in
+                            viewModel.selectedClub = selectedClub
+                        }
+                    )
+                    .frame(height: 415)
+                }
+        }
+    }
+    
+    // MARK: Student
+    @ViewBuilder
+    func inputStudentInfoView() -> some View {
+        VStack(spacing: 16) {
+            if !viewModel.yearOfAdmission.isEmpty {
+                BitgouelTextField(
+                    "학번",
+                    text: $viewModel.studentID,
+                    onSubmit: {
+                        focusField = .phoneNumber
+                    }
+                )
+                .focused($focusField, equals: .studentID)
+                .padding(.bottom, -20)
+            }
+            
+            if !viewModel.name.isEmpty {
+                BitgouelTextField(
+                    "입학년도",
+                    text: $viewModel.yearOfAdmission,
+                    onSubmit: {
+                        focusField = .studentID
+                    }
+                )
+                .focused($focusField, equals: .yearOfAdmission)
+                .padding(.bottom, -20)
+            }
         }
     }
 }
