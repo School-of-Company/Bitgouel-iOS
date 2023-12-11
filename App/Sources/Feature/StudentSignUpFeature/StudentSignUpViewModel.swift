08043a99a12ffb6d723d6672020ecf966b286b86
uuuunseo <jdbstjoo@gmail.com>
2023-12-11 23:30:05 +0900

    :bug: :: [#95] 병합 커밋

--- App/Sources/Feature/StudentSignUpFeature/StudentSignUpViewModel.swift
+++ App/Sources/Feature/StudentSignUpFeature/StudentSignUpViewModel.swift
@@ -1,7 +1,7 @@
 import Foundation
 import Service
 
-final class StudentSignUpViewModel: BaseViewModel {
+class StudentSignUpViewModel: BaseViewModel {
     @Published var schoolSearch = ""
     @Published var clubSearch = ""
     @Published var name = ""
@@ -27,7 +27,11 @@ final class StudentSignUpViewModel: BaseViewModel {
     @Published var emailTimeRemaining: Int = 180
     @Published var selectedSchool: String = "학교"
     @Published var selectedClub: String = "동아리"
+    @Published var selectedUniversity: String = ""
+    @Published var selectedGovernment: String = ""
+    @Published var selectedCompany: String = ""
     @Published var clubsForSelectedHighSchool: [String] = []
+    @Published var userRole: UserRoleType = .professor
     private var timer: Timer?
     private let studentSignupUseCase: StudentSignupUseCase
     private let teacherSignupUseCase: TeacherSignupUseCase
@@ -78,15 +82,52 @@ final class StudentSignUpViewModel: BaseViewModel {
     var titleMessage: String {
         if selectedSchool == "학교" {
             return "학교 선택"
-        } else if selectedClub == "동아리" {
-            return "동아리 선택"
-        } else if name.isEmpty {
-            return "이름 입력"
-        } else if yearOfAdmission.isEmpty {
-            return "입학년도 입력"
-        } else if studentID.isEmpty {
-            return "학번 입력"
-        } else if phoneNumber.isEmpty {
+        }
+        switch userRole {
+        case .student:
+            if selectedClub == "동아리" {
+                return "동아리 선택"
+            } else if name.isEmpty {
+                return "이름 입력"
+            } else if yearOfAdmission.isEmpty {
+                return "입학년도 입력"
+            } else if studentID.isEmpty {
+                return "학번 입력"
+            }
+        case .teacher, .bbozzack:
+            if selectedClub == "동아리" {
+                return "동아리 선택"
+            } else if name.isEmpty {
+                return "이름 입력"
+            }
+
+        case .companyInstructor:
+            if selectedClub == "동아리" {
+                return "동아리 선택"
+            } else if name.isEmpty {
+                return "이름 입력"
+            } else if selectedCompany.isEmpty {
+                return "기업 입력"
+            }
+
+        case .professor:
+            if selectedClub == "동아리" {
+                return "동아리 선택"
+            } else if name.isEmpty {
+                return "이름 입력"
+            } else if selectedUniversity.isEmpty {
+                return "대학 입력"
+            }
+
+        case .government:
+            if name.isEmpty {
+                return "이름 입력"
+            } else if selectedGovernment.isEmpty {
+                return "기관 입력"
+            }
+        }
+
+        if phoneNumber.isEmpty {
             return "전화번호 입력"
         } else if certificationNumberPhoneNumber.isEmpty {
             return "인증번호 입력"
@@ -102,17 +143,58 @@ final class StudentSignUpViewModel: BaseViewModel {
     }
 
     var subTitleMessage: String {
-        if selectedSchool == "학교" {
-            return "재학 중이신 학교를 선택해 주세요!"
-        } else if selectedClub == "동아리" {
-            return "가입하신 동아리를 선택해 주세요!"
-        } else if name.isEmpty {
-            return "이름을 입력해 주세요!"
-        } else if yearOfAdmission.isEmpty {
-            return "입학하신 연도를 입력해 주세요!"
-        } else if studentID.isEmpty {
-            return "학년, 반, 번호를 입력해 주세요! (ex: 1101)"
-        } else if phoneNumber.isEmpty {
+        switch userRole {
+        case .student:
+            if selectedSchool == "학교" {
+                return "재학 중이신 학교를 선택해 주세요!"
+            } else if selectedClub == "동아리" {
+                return "가입하신 동아리를 선택해 주세요!"
+            } else if name.isEmpty {
+                return "이름을 입력해 주세요!"
+            } else if yearOfAdmission.isEmpty {
+                return "입학하신 연도를 입력해 주세요!"
+            } else if studentID.isEmpty {
+                return "학년, 반, 번호를 입력해 주세요! (ex: 1101)"
+            }
+        case .teacher, .bbozzack:
+            if selectedSchool == "학교" {
+                return "담당 중이신 학교를 선택해 주세요!"
+            } else if selectedClub == "동아리" {
+                return "가입하신 동아리를 선택해 주세요!"
+            } else if name.isEmpty {
+                return "이름을 입력해 주세요!"
+            }
+        case .companyInstructor:
+            if selectedSchool == "학교" {
+                return "담당 중이신 학교를 선택해 주세요!"
+            } else if selectedClub == "동아리" {
+                return "가입하신 동아리를 선택해 주세요!"
+            } else if name.isEmpty {
+                return "이름을 입력해 주세요!"
+            } else if selectedCompany.isEmpty {
+                return "소속하신 기업을 입력해주세요!"
+            }
+        case .professor:
+            if selectedSchool == "학교" {
+                return "담당 중이신 학교를 선택해 주세요!"
+            } else if selectedClub == "동아리" {
+                return "가입하신 동아리를 선택해 주세요!"
+            } else if name.isEmpty {
+                return "이름을 입력해 주세요!"
+            } else if selectedUniversity.isEmpty {
+                return "소속하신 대학을 입력해주세요!"
+            }
+        case .government:
+            if selectedSchool == "학교" {
+                return "담당 중이신 학교를 선택해 주세요!"
+            } else if name.isEmpty {
+                return "이름을 입력해 주세요!"
+            } else if selectedGovernment.isEmpty {
+                return "소속하신 기관을 입력해주세요!"
+            }
+        }
+
+        if phoneNumber.isEmpty {
             return "인증을 위해 전화번호를 입력해 주세요!"
         } else if certificationNumberPhoneNumber.isEmpty {
             return "받으신 인증번호 N자리를 입력해 주세요!"
