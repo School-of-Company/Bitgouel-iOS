ca053f89a9b3d9ac3e67ca80d8cbc1cb576242b6
uuuunseo <jdbstjoo@gmail.com>
2023-12-18 12:04:40 +0900

    :sparkles: :: StudentActivityDetailEntity / Entity 추가

---
+++ Service/Sources/Domain/ActivityDomain/Entity/StudentActivityDetailEntity.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+public struct StudentActivityDetailEntity: Equatable {
+    public let id: UUID
+    public let title: String
+    public let content: String
+    public let credit: Int
+    public let activityDate: String
+    public let modifiedAt: String
+    
+    public init(id: UUID, title: String, content: String, credit: Int, activityDate: String, modifiedAt: String) {
+        self.id = id
+        self.title = title
+        self.content = content
+        self.credit = credit
+        self.activityDate = activityDate
+        self.modifiedAt = modifiedAt
+    }
+}
