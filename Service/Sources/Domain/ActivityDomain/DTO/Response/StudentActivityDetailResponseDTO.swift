4b5ce75e42208626bb9d1dadb20a68a22dddc9fb
uuuunseo <jdbstjoo@gmail.com>
2023-12-18 12:06:28 +0900

    :sparkles: :: ActivityDomain / StudentActivityDetailResponseDTO에 toDomain 추가

--- Service/Sources/Domain/ActivityDomain/DTO/Response/StudentActivityDetailResponseDTO.swift
+++ Service/Sources/Domain/ActivityDomain/DTO/Response/StudentActivityDetailResponseDTO.swift
@@ -1,10 +1,32 @@
 import Foundation
 
-public struct StudentActivityDetailResponseDTO {
+public struct StudentActivityDetailResponseDTO: Decodable {
     public let id: UUID
     public let title: String
     public let content: String
     public let credit: Int
     public let activityDate: String
     public let modifiedAt: String
+
+    public init(id: UUID, title: String, content: String, credit: Int, activityDate: String, modifiedAt: String) {
+        self.id = id
+        self.title = title
+        self.content = content
+        self.credit = credit
+        self.activityDate = activityDate
+        self.modifiedAt = modifiedAt
+    }
+}
+
+extension StudentActivityDetailResponseDTO {
+    func toDomain() -> StudentActivityDetailEntity {
+        StudentActivityDetailEntity(
+            id: id,
+            title: title,
+            content: content,
+            credit: credit,
+            activityDate: activityDate,
+            modifiedAt: modifiedAt
+        )
+    }
 }
