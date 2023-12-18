51bcfad34a965e03cf04986d902be46db3a497f5
uuuunseo <jdbstjoo@gmail.com>
2023-12-18 23:28:10 +0900

    :sparkles: :: [#101] DI / AppComponent+Activity 추가

--- App/Sources/Application/DI/Activity/AppComponent+Activity.swift
+++ App/Sources/Application/DI/Activity/AppComponent+Activity.swift
@@ -1,9 +1,58 @@
-//
-//  AppComponent+Activity.swift
-//  Bitgouel
-//
-//  Created by 정윤서 on 12/18/23.
-//  Copyright © 2023 team.msg. All rights reserved.
-//
+import NeedleFoundation
+import Service
 
-import Foundation
+public extension AppComponent {
+    var remoteDataSource: any RemoteActivityDataSource {
+        shared {
+            RemoteActivityDataSourceImpl(keychain: keychain)
+        }
+    }
+
+    var activityRepository: any ActivityRepository {
+        shared {
+            ActivityRepositoryImpl(remoteActivityDataSource: remoteDataSource)
+        }
+    }
+
+    var addStudentActivityUseCase: any AddStudentActivityUseCase {
+        shared {
+            AddStudentActivityUseCaseImpl(activityRepository: activityRepository)
+        }
+    }
+    
+    var approveStudentActivityUseCase: any ApproveStudentActivityUseCase {
+        shared {
+            ApproveStudentActivityUseCaseImpl(activityRepository: activityRepository)
+        }
+    }
+    
+    var deleteStudentActivityUseCase: any DeleteStudentActivityUseCase {
+        shared {
+            DeleteStudentActivityUseCaseImpl(activityRepository: activityRepository)
+        }
+    }
+    
+    var queryMyStudentActivityUseCase: any QueryMyStudentActivityUseCase {
+        shared {
+            QueryMyStudentActivityUseCaseImpl(activityRepository: activityRepository)
+        }
+    }
+    
+    var queryStudentActivityByIdUseCase: any QueryStudentActivityByIdUseCase {
+        shared {
+            QueryStudentActivityByIdUseCaseImpl(activityRepository: activityRepository)
+        }
+    }
+    
+    var queryStudentActivityListUseCase: any QueryStudentActivityListUseCase {
+        shared {
+            QueryStudentActivityListUseCaseImpl(activityRepository: activityRepository)
+        }
+    }
+    
+    var queryStudentActivityDetailsUseCase: any QueryStudentActivityDetailsUseCase {
+        shared {
+            QueryStudentActivityDetailsUseCaseImpl(activityRepository: activityRepository)
+        }
+    }
+}
