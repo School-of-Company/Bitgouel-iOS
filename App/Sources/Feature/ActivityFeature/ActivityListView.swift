fc19470f0bfb86cdb95c1e408bbb6a025d5d8db6
uuuunseo <jdbstjoo@gmail.com>
2023-12-26 10:11:12 +0900

    :recycle: :: [#101] Navigation / NavigationTitleView 삭제 -> toolbar로 수정

--- App/Sources/Feature/ActivityFeature/ActivityListView.swift
+++ App/Sources/Feature/ActivityFeature/ActivityListView.swift
@@ -2,33 +2,48 @@ import Service
 import SwiftUI
 
 struct ActivityListView: View {
+    @Environment(\.dismiss) var dismiss
     @StateObject var model: ActivityListModel
     @StateObject var viewModel: ActivityListViewModel
     var body: some View {
-        BitgouelNavigationTitleView(
-            title: "학생활동", buttonItems: [ButtonItem(image: "add", text: "")]) {
-                ScrollView {
-                    VStack {
-                        LazyVStack(spacing: 12) {
-                            ForEach(model.content, id: \.activityId) { item in
-                                StudentActivityRow(
-                                    activityId: item.activityId,
-                                    title: item.title,
-                                    date: item.activityDate,
-                                    userId: item.userId,
-                                    name: item.userName,
-                                    state: item.approveStatus,
-                                    authority: model.authority
-                                )
-                            }
+        NavigationView {
+            ScrollView {
+                VStack {
+                    LazyVStack(spacing: 12) {
+                        ForEach(model.content, id: \.activityId) { item in
+                            StudentActivityRow(
+                                activityId: item.activityId,
+                                title: item.title,
+                                date: item.activityDate,
+                                userId: item.userId,
+                                name: item.userName,
+                                state: item.approveStatus,
+                                authority: model.authority
+                            )
                         }
-                        .padding(.top, 8)
+                    }
+                    .padding(.top, 8)
+                }
+            }
+            .padding(.horizontal, 28)
+            .bitgouelBackButton(dismiss: dismiss)
+            .navigationTitle("학생활동").navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                ToolbarItemGroup(placement: .topBarTrailing) {
+                    if model.authority == .student {
+                        Button(action: {}, label: {
+                            Image(systemName: "plus")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 24, height: 24)
+                                .foregroundColor(.bitgouel(.greyscale(.g8)))
+                        })
                     }
                 }
             }
-            .padding(.top, 12)
-            .onAppear {
-                viewModel.onAppear()
-            }
+        }
+        .onAppear {
+            viewModel.onAppear()
+        }
     }
 }
