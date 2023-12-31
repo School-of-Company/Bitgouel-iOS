ba9ec2abd14f503a216a5ae552292728f0c6aeb3
uuuunseo <jdbstjoo@gmail.com>
2023-12-31 23:40:18 +0900

    :sparkles: :: [#102] InputActivityView / 레이아웃 수정 및버튼 추가

--- App/Sources/Feature/InputActivityFeature/InputActivityView.swift
+++ App/Sources/Feature/InputActivityFeature/InputActivityView.swift
@@ -1,57 +1,94 @@
 import SwiftUI
 
 struct InputActivityView: View {
-    @State var title: String = ""
-    @State var activityText: String = ""
+    @StateObject var viewModel: InputActivityViewModel
+    
+    init(viewModel: InputActivityViewModel) {
+        _viewModel = StateObject(wrappedValue: viewModel)
+    }
     
     var body: some View {
-        NavigationView {
+        ScrollView {
             VStack(spacing: 0) {
                 VStack {
-                    TextEditor(text: $title)
+                    TextEditor(text: $viewModel.title)
                         .bitgouelFont(.title3)
-                        .onChange(of: title, perform: { value in
-                            if title.count > 100 {
-                                title.removeLast()
-                            }
-                        })
                         .overlay(alignment: .topLeading) {
-                            if title.isEmpty {
+                            if viewModel.title.isEmpty {
                                 BitgouelText(
                                     text: "활동 제목 (100자 이내)",
-                                    font: .title3)
+                                    font: .title3
+                                )
+                                .padding(.top, 8)
+                                .padding(.leading, 4)
                                 .foregroundColor(.bitgouel(.greyscale(.g7)))
                             }
                         }
-                    Spacer()
+                        .onChange(of: viewModel.title, perform: { value in
+                            if viewModel.title.count > 100 {
+                                viewModel.title.removeLast()
+                            }
+                        })
                 }
-                .frame(maxHeight: 130)
+                .padding(.top, -40)
+                .frame(minHeight: 40)
+                .frame(maxHeight: 120)
                 
                 Divider()
                 
-                ScrollView {
-                    VStack {
-                        TextEditor(text: $activityText)
-                            .bitgouelFont(.text3)
-                            .overlay(alignment: .topLeading) {
-                                if activityText.isEmpty {
-                                    BitgouelText(text: "본문 입력 (1000자 이내)", font: .text3)
-                                        .foregroundColor(.bitgouel(.greyscale(.g7)))
-                                }
+                VStack {
+                    TextEditor(text: $viewModel.activityText)
+                        .bitgouelFont(.text3)
+                        .overlay(alignment: .topLeading) {
+                            if viewModel.activityText.isEmpty {
+                                BitgouelText(
+                                    text: "본문 입력 (1000자 이내)",
+                                    font: .text3
+                                )
+                                .padding(.top, 8)
+                                .padding(.leading, 4)
+                                .foregroundColor(.bitgouel(.greyscale(.g7)))
                             }
-                            .onChange(of: activityText, perform: { value  in
-                                if activityText.count > 1000 {
-                                    activityText.removeLast()
-                                }
-                            })
-                    }
+                        }
+                        .onChange(of: viewModel.activityText, perform: { value  in
+                            if viewModel.activityText.count > 1000 {
+                                viewModel.activityText.removeLast()
+                            }
+                        })
                 }
                 .padding(.top, 16)
+                .frame(height: 460)
                 
-                Spacer()
+                Divider()
+                
+                VStack(spacing: 8) {
+                    Button {
+                        
+                    } label: {
+                        Image("setting")
+                        
+                        Text("활동 세부 설정")
+                            .bitgouelFont(.text3, color: .primary(.p5))
+                    }
+                    .padding(.vertical, 12)
+                    .padding(.horizontal, 96)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8)
+                            .strokeBorder(Color.bitgouel(.primary(.p5)))
+                    }
+                    
+                    CTAButton(
+                        text: "활동 추가",
+                        style: .default
+                    )
+                }
+                .padding(.top, 24)
+                .padding(.bottom, 12)
             }
-            .padding(.horizontal, 28)
-            .padding(.top, 24)
+        }
+        .padding(.horizontal, 28)
+        .onTapGesture {
+            hideKeyboard()
         }
     }
 }
