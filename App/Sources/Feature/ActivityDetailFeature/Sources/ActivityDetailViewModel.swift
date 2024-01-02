ab727c3eadf936e90eb31aba0ea516d51f3db4c0
uuuunseo <jdbstjoo@gmail.com>
2024-01-02 20:24:06 +0900

    :sparkles: :: [#103] ActivityDetailView / BottomSheet 추가

---
+++ App/Sources/Feature/ActivityDetailFeature/Sources/ActivityDetailViewModel.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+import Service
+
+final class ActivityDetailViewModel: BaseViewModel {
+    @State var activityDate = Date()
+    @State private var isCredit: Bool = false
+    @Published var isPresentedCreditSheet: Bool = false
+    @Published var credit: Int = 0
+    let creditValue: [Int] = [1, 2]
+}
