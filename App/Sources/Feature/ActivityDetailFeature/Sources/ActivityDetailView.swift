662c407ec2939a464ad5f6ee8620b83ba7da5553
uuuunseo <jdbstjoo@gmail.com>
2024-01-03 17:33:44 +0900

    :sparkles: :: [#104] ActivityDetailView / 학생활동 상세 페이지 퍼블리싱

---
+++ App/Sources/Feature/ActivityDetailFeature/Sources/ActivityDetailView.swift
@@ -0,0 +1,88 @@
+import SwiftUI
+
+struct ActivityDetailView: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("상태")
+                    
+                    Spacer()
+                    
+                    Text("수정된 날짜")
+                    .foregroundColor(.bitgouel(.greyscale(.g7)))
+                }
+                
+                BitgouelText(
+                    text: "타이틀",
+                    font: .text1
+                )
+                
+                HStack {
+                    BitgouelText(
+                        text: "활동 날짜",
+                        font: .text3
+                    )
+                    
+                    Spacer()
+                    
+                    BitgouelText(
+                        text: "부여 학점",
+                        font: .text3
+                    )
+                }
+                .foregroundColor(.bitgouel(.greyscale(.g4)))
+            }
+            
+            ScrollView {
+                Text("활동")
+            }
+            .padding(.top, 24)
+            
+            ZStack {
+                popupButtonByWriter()
+            }
+        }
+        .padding(.horizontal, 28)
+    }
+    
+    @ViewBuilder
+    func popupButtonByTeacher() -> some View {
+        ZStack {
+            HStack {
+                CTAButton(
+                    text: "활동 거부",
+                    style: .error,
+                    action: {}
+                )
+                
+                Spacer()
+                
+                CTAButton(
+                    text: "활동 승인",
+                    style: .default,
+                    action: {}
+                )
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func popupButtonByWriter() -> some View {
+        HStack {
+            CTAButton(
+                text: "활동 수정",
+                style: .default,
+                action: {}
+            )
+            
+            Spacer()
+            
+            CTAButton(
+                text: "활동 삭제",
+                style: .error,
+                action: {}
+            )
+        }
+    }
+}
