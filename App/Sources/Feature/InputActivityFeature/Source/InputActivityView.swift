867aa6ca0d778ad81f5dc7ba5999f83e6e8a2208
uuuunseo <jdbstjoo@gmail.com>
2024-01-11 19:01:38 +0900

    :recycle: :: [#102] InputActivityView / TextEditor Text 부분 binding으로 변경

--- App/Sources/Feature/InputActivityFeature/Source/InputActivityView.swift
+++ App/Sources/Feature/InputActivityFeature/Source/InputActivityView.swift
@@ -2,70 +2,72 @@ import SwiftUI
 
 struct InputActivityView: View {
     @StateObject var viewModel: InputActivityViewModel
-
+    
     init(viewModel: InputActivityViewModel) {
         _viewModel = StateObject(wrappedValue: viewModel)
     }
-
+    
     var body: some View {
         ScrollView {
             VStack(spacing: 0) {
                 VStack {
-                    TextEditor(text: $viewModel.title)
-                        .bitgouelFont(.title3)
-                        .overlay(alignment: .topLeading) {
-                            if viewModel.title.isEmpty {
-                                BitgouelText(
-                                    text: "활동 제목 (100자 이내)",
-                                    font: .title3
-                                )
-                                .padding(.top, 8)
-                                .padding(.leading, 4)
-                                .foregroundColor(.bitgouel(.greyscale(.g7)))
-                            }
+                    TextEditor(text: Binding(
+                        get: { viewModel.title },
+                        set: { newValue in
+                            guard newValue.count <= 100 else { return }
+                            viewModel.title = newValue
                         }
-                        .onChange(of: viewModel.title, perform: { value in
-                            if viewModel.title.count > 100 {
-                                viewModel.title.removeLast()
-                            }
-                        })
+                    ))
+                    .bitgouelFont(.title3)
+                    .overlay(alignment: .topLeading) {
+                        if viewModel.title.isEmpty {
+                            BitgouelText(
+                                text: "활동 제목 (100자 이내)",
+                                font: .title3
+                            )
+                            .padding(.top, 8)
+                            .padding(.leading, 4)
+                            .foregroundColor(.bitgouel(.greyscale(.g7)))
+                        }
+                    }
                 }
                 .padding(.top, -40)
                 .frame(minHeight: 40, maxHeight: 120)
-
+                
                 Divider()
-
+                
                 VStack {
-                    TextEditor(text: $viewModel.activityText)
-                        .bitgouelFont(.text3)
-                        .overlay(alignment: .topLeading) {
-                            if viewModel.activityText.isEmpty {
-                                BitgouelText(
-                                    text: "본문 입력 (1000자 이내)",
-                                    font: .text3
-                                )
-                                .padding(.top, 8)
-                                .padding(.leading, 4)
-                                .foregroundColor(.bitgouel(.greyscale(.g7)))
-                            }
+                    TextEditor(text: Binding(
+                        get: { viewModel.activityText },
+                        set: { newValue in
+                            guard newValue.count <= 1000 else { return }
+                            viewModel.activityText = newValue
                         }
-                        .onChange(of: viewModel.activityText, perform: { value in
-                            if viewModel.activityText.count > 1000 {
-                                viewModel.activityText.removeLast()
-                            }
-                        })
+                    ))
+                    .bitgouelFont(.text3)
+                    .overlay(alignment: .topLeading) {
+                        if viewModel.activityText.isEmpty {
+                            BitgouelText(
+                                text: "본문 입력 (1000자 이내)",
+                                font: .text3
+                            )
+                            .padding(.top, 8)
+                            .padding(.leading, 4)
+                            .foregroundColor(.bitgouel(.greyscale(.g7)))
+                        }
+                    }
                 }
                 .padding(.top, 16)
                 .frame(height: 460)
-
+                
                 Divider()
-
+                
                 VStack(spacing: 8) {
                     Button {} label: {
                         Spacer()
                         
                         BitgouelAsset.Icons.add.swiftUIImage
-
+                        
                         Text("활동 세부 설정")
                             .bitgouelFont(.text3, color: .primary(.p5))
                         
@@ -77,7 +79,7 @@ struct InputActivityView: View {
                         RoundedRectangle(cornerRadius: 8)
                             .strokeBorder(Color.bitgouel(.primary(.p5)))
                     }
-
+                    
                     CTAButton(
                         text: "활동 추가",
                         style: .default
