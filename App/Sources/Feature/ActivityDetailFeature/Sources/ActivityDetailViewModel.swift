1cd3889368316a87ac455815e345a2df1af8255e
uuuunseo <jdbstjoo@gmail.com>
2024-01-13 14:47:13 +0900

    :bug: :: [#103] 충돌해결

--- App/Sources/Feature/ActivityDetailFeature/Sources/ActivityDetailViewModel.swift
+++ App/Sources/Feature/ActivityDetailFeature/Sources/ActivityDetailViewModel.swift
@@ -1,10 +1,10 @@
-import SwiftUI
 import Service
+import SwiftUI
 
 final class ActivityDetailViewModel: BaseViewModel {
     @State var activityDate = Date()
     @State private var isCredit: Bool = false
     @Published var isPresentedCreditSheet: Bool = false
-    @Published var credit: Int = 0
+    @Published var selectedCredit: Int = 0
     let creditValue: [Int] = [1, 2]
 }
