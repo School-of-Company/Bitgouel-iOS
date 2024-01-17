1c9acdb1de981bdfe21a0afa023cbff873a157cc
uuuunseo <jdbstjoo@gmail.com>
2024-01-17 22:33:48 +0900

    :sparkles: :: [#124] Data / 비밀번호 찾기, 내 정보 조회 UseCaseImpl 추가

---
+++ Service/Sources/Data/UseCase/User/ChangePasswordUseCaseImpl.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public struct ChangePasswordUseCaseImpl: ChangePasswordUseCase {
+    private let userRepository: any UserRepository
+    
+    public init(userRepository: any UserRepository) {
+        self.userRepository = userRepository
+    }
+    
+    public func callAsFunction(req: ChangePasswordRequestDTO) async throws {
+        userRepository.changePassword(req: req)
+    }
+}
