45085601a504b62cf2ce991c9ead9495e89427c2
uuuunseo <jdbstjoo@gmail.com>
2024-01-17 18:11:54 +0900

    :sparkles: :: [#103] InputActivityFeature / 학생 활동추가 기능추가

--- App/Sources/Feature/InputActivityFeature/Source/InputActivityViewModel.swift
+++ App/Sources/Feature/InputActivityFeature/Source/InputActivityViewModel.swift
@@ -2,11 +2,41 @@ import Foundation
 import Service
 
 final class InputActivityViewModel: BaseViewModel {
-    @Published var title: String
-    @Published var activityText: String
-
-    init(title: String, activityText: String) {
-        self.title = title
-        self.activityText = activityText
+    @Published var title: String = ""
+    @Published var activityText: String = ""
+    @Published var isPresentedDetailSettingAppend: Bool = false
+    @Published var activityDate: String = ""
+    @Published var activityCredit: Int = 0
+    
+    private let addStudentActivityUseCase: any AddStudentActivityUseCase
+    
+    init(addStudentActivityUseCase: any AddStudentActivityUseCase) {
+        self.addStudentActivityUseCase = addStudentActivityUseCase
+    }
+    
+    func detailSettingAppendDismissed() {
+        self.isPresentedDetailSettingAppend = false
+    }
+    
+    func updateActivityDetail(date: String, credit: Int) {
+        self.activityDate = date
+        self.activityCredit = credit
+    }
+    
+    func addActivity() {
+        Task {
+            do {
+                try await addStudentActivityUseCase(
+                    req: AddStudentActivityRequestDTO(
+                        title: title,
+                        content: activityText,
+                        credit: activityCredit,
+                        activityDate: activityDate
+                    )
+                )
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
 }
