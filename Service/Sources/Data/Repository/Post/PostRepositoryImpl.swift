3abc6dd112dcc1e73c12c435f0c77883a1e851e2
uuuunseo <jdbstjoo@gmail.com>
2024-01-21 03:16:51 +0900

    :sparkles: :: [#127] Data / PostRepositoryImpl

---
+++ Service/Sources/Data/Repository/Post/PostRepositoryImpl.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+public struct PostRepositoryImpl: PostRepository {
+    private let remotePostDataSource: RemotePostDataSource
+    
+    public init(remotePostDataSource: RemotePostDataSource) {
+        self.remotePostDataSource = remotePostDataSource
+    }
+    
+    public func writePost(req: InputPostRequestDTO) async throws {
+        try await remotePostDataSource.writePost(req: req)
+    }
+    
+    public func queryPostList() async throws -> [PostEntity] {
+        try await remotePostDataSource.queryPostList()
+    }
+    
+    public func queryPostDetail(postID: String) async throws -> PostDetailEntity {
+        try await remotePostDataSource.queryPostDetail(postID: postID)
+    }
+    
+    public func updatePost(postID: String, req: InputPostRequestDTO) async throws {
+        try await remotePostDataSource.updatePost(postID: postID, req: req)
+    }
+    
+    public func deletePost(postID: String) async throws {
+        try await remotePostDataSource.deletePost(postID: postID)
+    }
+}
--- Service/Sources/Domain/PostDomain/Repository/PostRepository.swift
+++ Service/Sources/Domain/PostDomain/Repository/PostRepository.swift
@@ -3,7 +3,7 @@ import Foundation
 public protocol PostRepository {
     func writePost(req: InputPostRequestDTO) async throws
     func queryPostList() async throws -> [PostEntity]
-    func queryPostDetail(postID: String) -> PostDetailEntity
+    func queryPostDetail(postID: String) async throws -> PostDetailEntity
     func updatePost(postID: String, req: InputPostRequestDTO) async throws
     func deletePost(postID: String) async throws
 }
