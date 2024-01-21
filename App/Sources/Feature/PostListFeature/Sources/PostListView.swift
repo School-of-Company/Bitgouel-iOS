0b085fcf60eee0f1ceb30be0d10adf1ec38d0559
uuuunseo <jdbstjoo@gmail.com>
2024-01-21 13:15:35 +0900

    :lipstick: :: [#129] PostListFeature / 게시글 목록 페이지 퍼블리싱

---
+++ App/Sources/Feature/PostListFeature/Sources/PostListView.swift
@@ -0,0 +1,52 @@
+import SwiftUI
+
+struct PostListView: View {
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 0) {
+                LazyVStack(spacing: 0) {
+                    ForEach(0...3, id: \.self) { index in
+                        PostListRow(
+                            postId: "index",
+                            title: "타이틀",
+                            modifedAt: "날짜"
+                        )
+                        
+                        Divider()
+                    }
+                }
+                
+                Spacer()
+            }
+                .padding(.horizontal, 28)
+                .navigationTitle("게시글 목록")
+                .toolbar {
+                    ToolbarItemGroup(placement: .navigationBarTrailing) {
+                        Button {} label: {
+                            BitgouelAsset.Icons.megaphone.swiftUIImage
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 24, height: 24)
+                        }
+
+                        Button {} label: {
+                            BitgouelAsset.Icons.questionmark.swiftUIImage
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 24, height: 24)
+                        }
+
+                        Button {} label: {
+                            Image(systemName: "plus")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 24, height: 24)
+                                .foregroundColor(.bitgouel(.greyscale(.g8)))
+                        }
+                    }
+                }
+
+            Spacer()
+        }
+    }
+}
