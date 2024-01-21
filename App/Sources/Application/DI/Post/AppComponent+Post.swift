1db9cf41c8555e2437732442847a48dbf544a1a8
uuuunseo <jdbstjoo@gmail.com>
2024-01-21 12:06:32 +0900

    :recycle: :: [#127] swiftFormat

--- App/Sources/Application/DI/Post/AppComponent+Post.swift
+++ App/Sources/Application/DI/Post/AppComponent+Post.swift
@@ -7,37 +7,37 @@ public extension AppComponent {
             RemotePostDataSourceImpl(keychain: keychain)
         }
     }
-    
+
     var postRepository: any PostRepository {
         shared {
             PostRepositoryImpl(remotePostDataSource: remotePostDataSource)
         }
     }
-    
+
     var writePostUseCase: any WritePostUseCase {
         shared {
             WritePostUseCaseImpl(postRepository: postRepository)
         }
     }
-    
+
     var queryPostListUseCase: any QueryPostListUseCase {
         shared {
             QueryPostListUseCaseImpl(postRepository: postRepository)
         }
     }
-    
+
     var queryPostDetailUseCase: any QueryPostDetailUseCase {
         shared {
             QueryPostDetailUseCaseImpl(postRepository: postRepository)
         }
     }
-    
+
     var updatePostUseCase: UpdatePostUseCase {
         shared {
             UpdatePostUseCaseImpl(postRepository: postRepository)
         }
     }
-    
+
     var deletePostUseCase: DeletePostUseCase {
         shared {
             DeletePostUseCaseImpl(postRepository: postRepository)
