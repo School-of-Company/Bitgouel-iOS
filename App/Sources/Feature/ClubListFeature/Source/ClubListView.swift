c04834c9b07f8e410cc905c59ec4a4cf804efe38
uuuunseo <jdbstjoo@gmail.com>
2024-01-26 01:27:10 +0900

    :lipstick: :: [#133] ClubListFeature / 동아리 목록 페이지 퍼블리싱

--- App/Sources/Feature/ClubListFeature/Source/ClubListView.swift
+++ App/Sources/Feature/ClubListFeature/Source/ClubListView.swift
@@ -1,9 +1,45 @@
-//
-//  ClubListView.swift
-//  Bitgouel
-//
-//  Created by 정윤서 on 1/23/24.
-//  Copyright © 2024 team.msg. All rights reserved.
-//
+import SwiftUI
 
-import Foundation
+struct ClubListView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 0) {
+                    HStack {
+                        BitgouelText(
+                            text: "학교이름",
+                            font: .title3
+                        )
+                        
+                        Spacer()
+                    }
+                    .padding(.top, 40)
+                
+                    Divider()
+                        .padding(.top, 12)
+                    
+                    ClubListRow(clubName: "동아리 이름")
+                        .padding(.top, 12)
+                    
+                    Spacer()
+                }
+                .padding(.horizontal, 28)
+            }
+            .navigationTitle("동아리 목록")
+            .toolbar {
+                ToolbarItemGroup(placement: .topBarTrailing) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        BitgouelAsset.Icons.setting.swiftUIImage
+                            .renderingMode(.template)
+                            .frame(width: 24, height: 24)
+                            .foregroundColor(.bitgouel(.greyscale(.g8)))
+                    }
+                }
+            }
+        }
+    }
+}
