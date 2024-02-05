85f3903706ee2a2de78bec9a62993e650d3a5529
uuuunseo <jdbstjoo@gmail.com>
2024-02-05 12:57:45 +0900

    :sparkles: :: [#142] ClubDomain / StudentDetailByClubEntity추가

---
+++ Service/Sources/Domain/ClubDomain/Entity/StudentDetailByClubEntity.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+public struct StudentDetailByClubEntity: Equatable {
+    public let name: String
+    public let phoneNumber: String
+    public let email: String
+    public let credit: Int
+    
+    init(
+        name: String,
+        phoneNumber: String,
+        email: String,
+        credit: Int
+    ) {
+        self.name = name
+        self.phoneNumber = phoneNumber
+        self.email = email
+        self.credit = credit
+    }
+}
