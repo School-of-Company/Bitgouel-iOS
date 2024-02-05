4c47705b048abc0e47a186bf9b13554f8d2fa1e6
uuuunseo <jdbstjoo@gmail.com>
2024-02-05 12:51:24 +0900

    :recycle: :: [#142] ClubDomain / Single 삭제

--- Service/Sources/Domain/ClubDomain/Entity/ClubDetailEntity.swift
+++ Service/Sources/Domain/ClubDomain/Entity/ClubDetailEntity.swift
@@ -5,7 +5,11 @@ public struct ClubDetailEntity: Equatable {
     public let highSchoolName: String
     public let studentHeadcount: Int
 
-    public init(clubName: String, highSchoolName: String, studentHeadcount: Int) {
+    public init(
+        clubName: String,
+        highSchoolName: String,
+        studentHeadcount: Int
+    ) {
         self.clubName = clubName
         self.highSchoolName = highSchoolName
         self.studentHeadcount = studentHeadcount
