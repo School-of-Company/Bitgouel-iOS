6a6c29b688cc9b68ddd01ae16264c824a1cc0b9c
uuuunseo <jdbstjoo@gmail.com>
2024-02-05 12:52:59 +0900

    :sparkles: :: [#142] ClubDomain / StudentDetailByClubResponseDTO 추가

--- Service/Sources/Domain/ClubDomain/DTO/Response/StudentDetailByClubResponseDTO.swift
+++ Service/Sources/Domain/ClubDomain/DTO/Response/StudentDetailByClubResponseDTO.swift
@@ -1,8 +1,20 @@
-//
-//  StudentDetailByClubResponseDTO.swift
-//  Service
-//
-//  Created by 정윤서 on 2/5/24.
-//
-
 import Foundation
+
+public struct StudentDetailByClubResponseDTO: Decodable {
+    public let name: String
+    public let phoneNumber: String
+    public let email: String
+    public let credit: Int
+
+    init(
+        name: String,
+        phoneNumber: String,
+        email: String,
+        credit: Int
+    ) {
+        self.name = name
+        self.phoneNumber = phoneNumber
+        self.email = email
+        self.credit = credit
+    }
+}
