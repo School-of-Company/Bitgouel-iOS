6de5fbecf0c129b56d460d24111a5f2883d01c21
uuuunseo <jdbstjoo@gmail.com>
2024-02-14 22:04:18 +0900

    :recycle: :: [#148] Utils / toBitgouelDate 옮기기

--- Service/Sources/Base/DataSource/BaseRemoteDataSource.swift
+++ Service/Sources/Base/DataSource/BaseRemoteDataSource.swift
@@ -132,12 +132,3 @@ private extension _Concurrency.Task where Failure == Error {
         }
     }
 }
-
-extension String {
-    func toBitgouelDate() -> Date {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
-        formatter.locale = Locale(identifier: "ko_kr")
-        return formatter.date(from: self) ?? .init()
-    }
-}
