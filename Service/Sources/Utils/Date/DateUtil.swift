462d587ea470699f23ed9882058f6b02774ea68c
uuuunseo <jdbstjoo@gmail.com>
2024-02-20 02:26:40 +0900

    :sparkles: :: [#150] Utils / DateFormatter Util 추가

--- Service/Sources/Utils/Date/DateUtil.swift
+++ Service/Sources/Utils/Date/DateUtil.swift
@@ -7,4 +7,46 @@ public extension String {
         formatter.locale = Locale(identifier: "ko_kr")
         return formatter.date(from: self) ?? .init()
     }
+
+    func toISODate(
+        options: ISO8601DateFormatter.Options = [
+            .withFullDate,
+            .withTime,
+            .withDashSeparatorInDate,
+            .withColonSeparatorInTime
+        ],
+        timeZone: TimeZone = .current
+    ) -> Date {
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = options
+        formatter.timeZone = timeZone
+        return formatter.date(from: self) ?? .init()
+    }
+
+    func toDateCustomFormat(format: String) -> Date {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.date(from: self) ?? .init()
+    }
+}
+
+public extension Date {
+    func toISOString(
+        options: ISO8601DateFormatter.Options = [
+            .withFullDate,
+            .withTime,
+            .withDashSeparatorInDate,
+            .withColonSeparatorInTime
+        ]
+    ) -> String {
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = options
+        return formatter.string(from: self)
+    }
+
+    func toStringCustomFormat(format: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.string(from: self)
+    }
 }
