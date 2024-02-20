e56f6fd29a933c6f05963748c8d2d89b9b131953
uuuunseo <jdbstjoo@gmail.com>
2024-02-21 03:13:01 +0900

    :lipstick: :: [#131] PostDetailSettingFeature / PostDetailView 수정

--- App/Sources/Feature/PostDetailSettingFeature/Source/PostDetailSettingView.swift
+++ App/Sources/Feature/PostDetailSettingFeature/Source/PostDetailSettingView.swift
@@ -2,49 +2,57 @@ import SwiftUI
 
 struct PostDetailSettingView: View {
     @Environment(\.dismiss) var dismiss
-    @State private var links: [String?] = Array(repeating: nil, count: 4)
-    @State var link: String = ""
-    @State var postId: Int = 4
+    @StateObject var viewModel: PostDetailSettingViewModel
     
     var body: some View {
         NavigationView {
             VStack(spacing: 0) {
-                HStack {
-                    BitgouelText(
-                        text: "링크 입력",
-                        font: .text1
-                    )
-                    
-                    Spacer()
-                }
-                .padding(.top, 28)
-                
-                VStack {
-                    ForEach(0...3, id: \.self) { index in
-                        BitgouelTextField(
-                            "링크 입력 (선택)",
-                            text: Binding(
-                                get: { links[index] ?? "" },
-                                set: { newValue in
-                                    links[index] = newValue
-                                }
-                            )
+                ScrollView {
+                    HStack {
+                        BitgouelText(
+                            text: "링크 입력",
+                            font: .text1
                         )
+                        
+                        Spacer()
                     }
+                    .padding(.top, 28)
+                    
+                    VStack {
+                        linkRow()
+                    }
+                    .padding(.top, 8)
+                    
+                    Button {
+                        viewModel.links.append("")
+                    } label: {
+                        HStack {
+                            Text("링크 추가")
+                            
+                            Spacer()
+                            
+                            BitgouelAsset.Icons.add.swiftUIImage
+                                .renderingMode(.template)
+                        }
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 16)
+                        .foregroundColor(.bitgouel(.greyscale(.g10)))
+                        .background(Color.bitgouel(.primary(.p5)))
+                        .cornerRadius(8, corners: .allCorners)
+                    }
+                    .padding(.top, 8)
+                    
                 }
-                .padding(.top, 8)
-                
                 Spacer()
                 
                 CTAButton(text: "적용하기") {
                     dismiss()
                 }
                 .padding(.bottom, 12)
-                
             }
             .padding(.horizontal, 28)
             .navigationTitle("게시글 세부설정")
-            .toolbar { 
+            .toolbar {
                 ToolbarItemGroup(placement: .navigationBarTrailing) {
                     Button {
                         dismiss()
@@ -53,6 +61,20 @@ struct PostDetailSettingView: View {
                     }
                 }
             }
+        }
+    }
+
+    @ViewBuilder
+    func linkRow() -> some View {
+        ForEach(viewModel.links.indices, id: \.self) { index in
+            BitgouelTextField(
+                "링크 입력 (선택)",
+                text: Binding(
+                    get: { viewModel.links[index] },
+                    set: { newvalue in
+                        viewModel.links[index] = newvalue
+                })
+            )
         }
     }
 }
