3cf63be4a530fb2af7b2702453f06cd5b75e9f7c
uuuunseo <jdbstjoo@gmail.com>
2024-02-21 12:35:37 +0900

    :sparkles: :: [#150] DesignSystem / LinkDetailSettingView 추가

---
+++ App/Sources/DesignSystem/View/LinkDetailSettingView.swift
@@ -0,0 +1,67 @@
+import SwiftUI
+
+struct LinkDetailSettingView: View {
+    @State var links: [String]
+    let link: String
+    let dismiss: DismissAction
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            ScrollView {
+                HStack {
+                    BitgouelText(
+                        text: "링크 입력",
+                        font: .text1
+                    )
+                    
+                    Spacer()
+                }
+                .padding(.top, 28)
+                
+                VStack {
+                    linkRow()
+                }
+                .padding(.top, 8)
+                
+                Button {
+                    links.append("")
+                } label: {
+                    HStack {
+                        Text("링크 추가")
+                        
+                        Spacer()
+                        
+                        BitgouelAsset.Icons.add.swiftUIImage
+                            .renderingMode(.template)
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 16)
+                    .foregroundColor(.bitgouel(.greyscale(.g10)))
+                    .background(Color.bitgouel(.primary(.p5)))
+                    .cornerRadius(8, corners: .allCorners)
+                }
+                .padding(.top, 8)
+                
+            }
+            Spacer()
+            
+            CTAButton(text: "적용하기") {
+                dismiss()
+            }
+            .padding(.bottom, 12)
+        }
+    }
+    
+    @ViewBuilder
+    func linkRow() -> some View {
+        ForEach(links.indices, id: \.self) { index in
+            BitgouelTextField(
+                "링크 입력 (선택)",
+                text: Binding(
+                    get: { links[index] },
+                    set: { newvalue in links[index] = newvalue }
+                )
+            )
+        }
+    }
+}
