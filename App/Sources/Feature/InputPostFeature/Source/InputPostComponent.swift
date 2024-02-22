fe3a5643515c58f6b09d0a7e5aef2c63d9716448
uuuunseo <jdbstjoo@gmail.com>
2024-02-22 23:55:20 +0900

    :sparkles: :: [#162] InputPostFeature / DetailSettingView fullScreenCover 추가

--- App/Sources/Feature/InputPostFeature/Source/InputPostComponent.swift
+++ App/Sources/Feature/InputPostFeature/Source/InputPostComponent.swift
@@ -3,11 +3,11 @@ import SwiftUI
 import Service
 
 public protocol InputPostDependency: Dependency {
-    
+    var postDetailSettingFactory: any PostDetailSettingFactory { get }
 }
 
 public final class InputPostComponent: Component<InputPostDependency>, InputPostFactory {
     public func makeView() -> some View {
-        InputPostView()
+        InputPostView(postDetailSettingFactory: dependency.postDetailSettingFactory)
     }
 }
--- App/Sources/Feature/InputPostFeature/Source/InputPostView.swift
+++ App/Sources/Feature/InputPostFeature/Source/InputPostView.swift
@@ -3,29 +3,52 @@ import SwiftUI
 struct InputPostView: View {
     @State var postTitle: String = ""
     @State var postText: String = ""
+    @State var isPresentedPostDetailSettingAppend: Bool = false
+    @State var postRelatedLink: [String] = []
+    
+    private let postDetailSettingFactory: any PostDetailSettingFactory
+    
+    init(postDetailSettingFactory: any PostDetailSettingFactory) {
+        self.postDetailSettingFactory = postDetailSettingFactory
+    }
     
     var body: some View {
-        VStack(spacing: 0) {
-            InputFormView(
-                epic: "게시글",
-                state: "추가",
-                settingButtonAction: {
-                    
-                },
-                finalButtonAction: {
-                    
-                },
-                title: Binding(get: {
-                    postTitle
-                }, set: { title in
-                    postTitle = title
-                }),
-                text: Binding(get: {
-                    postText
-                }, set: { text in
-                    postText = text
-                })
+        ScrollView {
+            VStack(spacing: 0) {
+                InputFormView(
+                    epic: "게시글",
+                    state: "추가",
+                    settingButtonAction: {
+                        isPresentedPostDetailSettingAppend = true
+                    },
+                    finalButtonAction: {
+                        
+                    },
+                    title: Binding(get: {
+                        postTitle
+                    }, set: { title in
+                        postTitle = title
+                    }),
+                    text: Binding(get: {
+                        postText
+                    }, set: { text in
+                        postText = text
+                    })
+                )
+            }
+        }
+        .fullScreenCover(
+            isPresented: Binding(
+                get: { isPresentedPostDetailSettingAppend },
+                set: { _ in isPresentedPostDetailSettingAppend = false }
             )
+        ) {
+            DeferView {
+                postDetailSettingFactory.makeView(links: postRelatedLink) {
+                    links in
+                    postRelatedLink = links
+                }.eraseToAnyView()
+            }
         }
     }
 }
