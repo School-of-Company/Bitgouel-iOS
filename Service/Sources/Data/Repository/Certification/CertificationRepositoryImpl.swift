3a20714160e8b93d308a50fff19d3fc1c6ac985c
uuuunseo <jdbstjoo@gmail.com>
2024-02-24 20:18:32 +0900

    :sparkles: :: [#172] CertificationData / repository

---
+++ Service/Sources/Data/Repository/Certification/CertificationRepositoryImpl.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+public struct CertificationRepositoryImpl: CertificationRepository {
+    private let remoteCertificationDataSource: any RemoteCertificationDataSource
+    
+    init(remoteCertificationDataSource: any RemoteCertificationDataSource) {
+        self.remoteCertificationDataSource = remoteCertificationDataSource
+    }
+    
+    public func queryCertificationListByTeacher(studentID: String) async throws -> [CertificationInfoEntity] {
+        try await remoteCertificationDataSource.queryCertificationListByTeacher(studentID: studentID)
+    }
+    
+    public func queryCertificationListByStudent() async throws -> [CertificationInfoEntity] {
+        try await remoteCertificationDataSource.queryCertificationListByStudent()
+    }
+    
+    public func inputCertification(req: InputCertificationRequestDTO) async throws {
+        try await remoteCertificationDataSource.inputCertification(req: req)
+    }
+    
+    public func updateCertification(certificationID: String, req: InputCertificationRequestDTO) async throws {
+        try await remoteCertificationDataSource.updateCertification(certificationID: certificationID, req: req)
+    }
+}
