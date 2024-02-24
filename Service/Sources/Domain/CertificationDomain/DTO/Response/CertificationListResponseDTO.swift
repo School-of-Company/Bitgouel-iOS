68add465b49c1afee57a9ee6cc65bbb6a6140618
uuuunseo <jdbstjoo@gmail.com>
2024-02-24 14:41:38 +0900

    :sparkles: :: [#172] CertificationDomain / DTO

--- Service/Sources/Domain/CertificationDomain/DTO/Response/CertificationListResponseDTO.swift
+++ Service/Sources/Domain/CertificationDomain/DTO/Response/CertificationListResponseDTO.swift
@@ -1,8 +1,26 @@
-//
-//  CertificationListResponseDTO.swift
-//  Service
-//
-//  Created by 정윤서 on 2/24/24.
-//
-
 import Foundation
+
+public struct CertificationListResponseDTO: Decodable {
+    public let certifications: [CertificationInfo]
+    
+    public init(certifications: [CertificationInfo]) {
+        self.certifications = certifications
+    }
+
+    public struct CertificationInfo: Decodable {
+        public let certificationId: String
+        public let name: String
+        public let acquisitionDate: String
+
+        public init(
+            certificationId: String,
+            name: String,
+            acquisitionDate: String
+        ) {
+            self.certificationId = certificationId
+            self.name = name
+            self.acquisitionDate = acquisitionDate
+        }
+    }
+}
+
