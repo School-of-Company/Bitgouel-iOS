33f442f25633989fa0aaee68ba2f9d7d0235a413
uuuunseo <105629604+uuuunseo@users.noreply.github.com>
2024-02-26 16:45:07 +0900

    Update App/Sources/Feature/InputCertificationFeature/Source/InputCertificationComponent.swift
    Co-authored-by: baegteun <baegteun@gmail.com>

--- App/Sources/Feature/InputCertificationFeature/Source/InputCertificationComponent.swift
+++ App/Sources/Feature/InputCertificationFeature/Source/InputCertificationComponent.swift
@@ -4,7 +4,7 @@ import SwiftUI
 
 public protocol InputCertificationDependency: Dependency {}
 
-public final class InputCertificationComponent: Component<InputCertificationDependency> , InputCertificationFactory {
+public final class InputCertificationComponent: Component<InputCertificationDependency>, InputCertificationFactory {
     public func makeView() -> some View {
         InputCertificationView(
             viewModel: .init()
