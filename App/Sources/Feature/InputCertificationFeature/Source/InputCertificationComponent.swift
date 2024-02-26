c70ae2e8feb6a26751e9699d88eaaae81749af9d
uuuunseo <jdbstjoo@gmail.com>
2024-02-26 17:26:52 +0900

    :sparkles: :: InputCertificationFeature / 자격증 수정 기능 추가

--- App/Sources/Feature/InputCertificationFeature/Source/InputCertificationComponent.swift
+++ App/Sources/Feature/InputCertificationFeature/Source/InputCertificationComponent.swift
@@ -4,13 +4,15 @@ import SwiftUI
 
 public protocol InputCertificationDependency: Dependency {
     var inputCertificationUseCase: any InputCertificationUseCase { get }
+    var updateCertificationUseCase: any UpdateCertificationUseCase { get }
 }
 
 public final class InputCertificationComponent: Component<InputCertificationDependency>, InputCertificationFactory {
     public func makeView(epic: String) -> some View {
         InputCertificationView(
             viewModel: .init(
-                inputCertificationUseCase: dependency.inputCertificationUseCase, 
+                inputCertificationUseCase: dependency.inputCertificationUseCase,
+                updateCertificationUseCase: dependency.updateCertificationUseCase,
                 epic: epic
             )
         )
--- App/Sources/Feature/InputCertificationFeature/Source/InputCertificationViewModel.swift
+++ App/Sources/Feature/InputCertificationFeature/Source/InputCertificationViewModel.swift
@@ -5,14 +5,18 @@ final class InputCertificationViewModel: BaseViewModel {
     @Published var certificationName: String = ""
     @Published var acquisitionDate = Date()
     var epic: String = ""
+    var certificationID: String = ""
 
     private let inputCertificationUseCase: any InputCertificationUseCase
+    private let updateCertificationUseCase: any UpdateCertificationUseCase
     
     init(
         inputCertificationUseCase: any InputCertificationUseCase,
+        updateCertificationUseCase: any UpdateCertificationUseCase,
         epic: String
     ) {
         self.inputCertificationUseCase = inputCertificationUseCase
+        self.updateCertificationUseCase = updateCertificationUseCase
         self.epic = epic
     }
 
@@ -23,13 +27,37 @@ final class InputCertificationViewModel: BaseViewModel {
     func applyButtonDidTap() {
         Task {
             do {
-                try await inputCertificationUseCase(
-                    req: InputCertificationRequestDTO(
-                        name: certificationName,
-                        acquisitionDate: acquisitionDate.toStringCustomFormat(format: "yyyy-MM-dd")
-                    )
-                )
+                switch epic {
+                case "등록":
+                    return try await inputCertification()
+                    
+                case "수정":
+                    return try await updateCertification()
+                    
+                default:
+                    return
+                }
+            } catch {
+                print(error.localizedDescription)
             }
         }
+    }
+    
+    func inputCertification() async throws {
+        try await inputCertificationUseCase(
+            req: InputCertificationRequestDTO(
+                name: certificationName,
+                acquisitionDate: acquisitionDate.toStringCustomFormat(format: "yyyy-MM-dd")
+            ))
+    }
+    
+    func updateCertification() async throws {
+        try await updateCertificationUseCase(
+            certificationID: certificationID,
+            req: InputCertificationRequestDTO(
+                name: certificationName,
+                acquisitionDate: acquisitionDate.toStringCustomFormat(format: "yyyy-MM-dd")
+            )
+        )
     }
 }
