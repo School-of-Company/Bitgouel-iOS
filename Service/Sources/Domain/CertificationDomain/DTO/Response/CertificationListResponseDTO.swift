eed1fae273d320e96154fac1d6e37ad719c32be4
uuuunseo <jdbstjoo@gmail.com>
2024-02-27 13:32:20 +0900

    :sparkles: :: clubID 추가

--- Service/Sources/Domain/CertificationDomain/DTO/Response/CertificationListResponseDTO.swift
+++ Service/Sources/Domain/CertificationDomain/DTO/Response/CertificationListResponseDTO.swift
@@ -10,12 +10,12 @@ public struct CertificationListResponseDTO: Decodable {
     public struct CertificationInfo: Decodable {
         public let certificationID: String
         public let name: String
-        public let acquisitionDate: String
+        public let acquisitionDate: Date
 
         public init(
             certificationID: String,
             name: String,
-            acquisitionDate: String
+            acquisitionDate: Date
         ) {
             self.certificationID = certificationID
             self.name = name
