558c002fc2e427e6505bd34bc7c9886a001a4a2d
uuuunseo <jdbstjoo@gmail.com>
2024-02-29 02:17:23 +0900

    :sparkles: :: [#190] InquiryDomain / Error

--- Service/Sources/Domain/InquiryDomain/Error/InquiryDomainError.swift
+++ Service/Sources/Domain/InquiryDomain/Error/InquiryDomainError.swift
@@ -1,8 +1,20 @@
-//
-//  InquiryDomainError.swift
-//  Service
-//
-//  Created by 정윤서 on 2/29/24.
-//
-
 import Foundation
+
+public enum InquiryDomainError: Error {
+    case forbidden
+    case conflict
+    case invalidRequest
+}
+
+extension InquiryDomainError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .forbidden:
+            return "권한이 없습니다."
+        case .conflict:
+            return "같은 문의사항이 존재합니다."
+        case .invalidRequest:
+            return "알 수 없는 에러가 발생하였습니다."
+        }
+    }
+}
