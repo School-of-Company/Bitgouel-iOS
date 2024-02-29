3419c45949c841012a0eb47a655197c7250b8286
uuuunseo <jdbstjoo@gmail.com>
2024-02-29 15:37:29 +0900

    :sparkles: :: [#190] InquiryDomain / AppComponent

--- Service/Sources/Data/UseCase/Inquiry/FetchInquiryDetailUseCaseImpl.swift
+++ Service/Sources/Data/UseCase/Inquiry/FetchInquiryDetailUseCaseImpl.swift
@@ -2,11 +2,11 @@ import Foundation
 
 public struct FetchInquiryDetailUseCaseImpl: FetchInquiryDetailUseCase {
     private let inquiryRepository: InquiryRepository
-    
+
     public init(inquiryRepository: InquiryRepository) {
         self.inquiryRepository = inquiryRepository
     }
-    
+
     public func callAsFunction(inquiryID: String) async throws -> InquiryDetailEntity {
         try await inquiryRepository.fetchInquiryDetail(inquiryID: inquiryID)
     }
