eaf31937e938620c2d7f1e859d08508d1d732091
uuuunseo <jdbstjoo@gmail.com>
2024-02-29 15:37:29 +0900

    :sparkles: :: [#190] InquiryDomain / AppComponent

--- Service/Sources/Domain/InquiryDomain/DTO/Request/InputInquiryRequestDTO.swift
+++ Service/Sources/Domain/InquiryDomain/DTO/Request/InputInquiryRequestDTO.swift
@@ -3,7 +3,7 @@ import Foundation
 public struct InputInquiryRequestDTO: Encodable {
     public let question: String
     public let questionDetail: String
-    
+
     public init(
         question: String,
         questionDetail: String
