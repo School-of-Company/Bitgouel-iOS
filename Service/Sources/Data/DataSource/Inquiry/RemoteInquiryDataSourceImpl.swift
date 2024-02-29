e9fbaf06c292633549f446da1d2fbe2a060dc72b
uuuunseo <jdbstjoo@gmail.com>
2024-02-29 14:59:42 +0900

    :sparkles: :: [#190] InquiryDomain / DataSourceImpl

---
+++ Service/Sources/Data/DataSource/Inquiry/RemoteInquiryDataSourceImpl.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+public final class RemoteInquiryDataSourceImpl: BaseRemoteDataSource<InquiryAPI>, RemoteInquiryDataSource {
+    public func inputInquiry(req: InputInquiryRequestDTO) async throws {
+        try await request(.inputInquiry(req: req))
+    }
+
+    public func fetchMyInquiryList() async throws -> [InquiryInfoEntity] {
+        try await request(.fetchMyInquiryList, dto: FetchInquiryListResponseDTO.self).toDomain()
+    }
+    
+    public func fetchInquiryDetail(inquiryID: String) async throws -> InquiryDetailEntity {
+        try await request(.fetchInquiryDetail(inquiryID: inquiryID), dto: FetchInquiryDetailResponseDTO.self).toDomain()
+    }
+    
+    public func deleteMyInquiry(inquiryID: String) async throws {
+        try await request(.deleteMyInquiry(inquiryID: inquiryID))
+    }
+    
+    public func modifyMyInquiry(inquiryID: String, req: InputInquiryRequestDTO) async throws {
+        try await request(.modifyMyInquiry(inquiryID: inquiryID, req: req))
+    }
+    
+    public func replyInquiry(inquiryID: String, answer: String) async throws {
+        try await request(.replyInquiry(inquiryID: inquiryID, answer: answer))
+    }
+    
+    public func fetchInquiryListByAdmin(answerStatus: AnswerStatusType, keyword: String) async throws -> [InquiryInfoEntity] {
+        try await request(.fetchInquiryListByAdmin(answerStatus: answerStatus, keyword: keyword), dto: FetchInquiryListResponseDTO.self).toDomain()
+    }
+    
+    public func deleteInquiryByAdmin(inquiryID: String) async throws {
+        try await request(.deleteMyInquiry(inquiryID: inquiryID))
+    }
+}
