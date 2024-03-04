058acbd26a8304ab0a7779320d7984f6593a37a7
uuuunseo <jdbstjoo@gmail.com>
2024-03-05 08:15:01 +0900

    :bento: :: [#111] Icon / home, laptop, message_fill, person3_fill  추가

---
+++ App/Sources/Feature/MainTabFeature/Interface/MainTabFactory.swift
@@ -0,0 +1,9 @@
+//
+//  MainTabFactory.swift
+//  Bitgouel
+//
+//  Created by 정윤서 on 3/5/24.
+//  Copyright © 2024 team.msg. All rights reserved.
+//
+
+import Foundation
---
+++ App/Sources/Feature/MainTabFeature/Source/MainTabComponent.swift
@@ -0,0 +1,9 @@
+//
+//  MainTabComponent.swift
+//  Bitgouel
+//
+//  Created by 정윤서 on 3/5/24.
+//  Copyright © 2024 team.msg. All rights reserved.
+//
+
+import Foundation
