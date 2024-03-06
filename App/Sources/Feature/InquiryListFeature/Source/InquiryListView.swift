89c01ff14e71d68677216e51b1836ab1e76c8acc
uuuunseo <jdbstjoo@gmail.com>
2024-03-06 12:28:51 +0900

    :lipstick: :: [#205] InquiryListFeature / 문의사항 리스트 페이지 퍼블리싱

--- App/Sources/Feature/InquiryListFeature/Source/InquiryListView.swift
+++ App/Sources/Feature/InquiryListFeature/Source/InquiryListView.swift
@@ -2,6 +2,40 @@ import SwiftUI
 
 struct InquiryListView: View {
     var body: some View {
-        Text("InquiryListView")
+        ScrollView {
+            VStack(spacing: 12) {
+                ForEach(0...3, id: \.self) { id in
+                    RoundListRow(
+                        id: "",
+                        title: "앗싸",
+                        date: "아아악",
+                        userID: "idid",
+                        name: "name",
+                        state: .approve,
+                        authority: .student
+                    )
+                }
+            }
+            .navigationTitle("문의사항")
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        #warning("학생일 때")
+                        BitgouelAsset.Icons.add.swiftUIImage
+                        
+                        #warning("어드민 일 때")
+                        BitgouelAsset.Icons.filter.swiftUIImage
+                    }
+                }
+            }
+            
+        }
+        .padding(.horizontal, 28)
     }
+}
+
+#Preview {
+    InquiryListView()
 }
