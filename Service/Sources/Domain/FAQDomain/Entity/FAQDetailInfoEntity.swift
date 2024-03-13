f117a92416eb11b9f92a81a984131de3850032fe
uuuunseo <jdbstjoo@gmail.com>
2024-03-13 11:39:56 +0900

    :sparkles: :: [#221] FAQDomain / Entity

---
+++ Service/Sources/Domain/FAQDomain/Entity/FAQDetailInfoEntity.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public struct FAQDetailInfoEntity: Equatable {
+    public let questionID: String
+    public let question: String
+    public let answer: String
+    
+    public init(
+        questionID: String,
+        question: String,
+        answer: String
+    ) {
+        self.questionID = questionID
+        self.question = question
+        self.answer = answer
+    }
+}
---
+++ Service/Sources/Domain/FAQDomain/Entity/FAQInfoEntity.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+public struct FAQInfoEntity: Equatable {
+    public let questionID: String
+    public let question: String
+    
+    public init(
+        questionID: String,
+        question: String
+    ) {
+        self.questionID = questionID
+        self.question = question
+    }
+}
