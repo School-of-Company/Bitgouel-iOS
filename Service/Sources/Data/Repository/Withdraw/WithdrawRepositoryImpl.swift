35ab852612403caac1e9a2928b11c56890c76eca
uuuunseo <jdbstjoo@gmail.com>
2024-03-13 15:00:03 +0900

    :sparkles: :: [#223] WithdrawDomain / RepositoryImpl

---
+++ Service/Sources/Data/Repository/Withdraw/WithdrawRepositoryImpl.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public struct WithdrawRepositoryImpl: WithdrawRepository {
+    private let remoteWithdrawDataSource: RemoteWithdrawDataSource
+    
+    public init(remoteWithdrawDataSource: RemoteWithdrawDataSource) {
+        self.remoteWithdrawDataSource = remoteWithdrawDataSource
+    }
+    
+    public func fetchWithdrawUserList(cohort: String) async throws -> [WithdrawUserInfoEntity] {
+        try await remoteWithdrawDataSource.fetchWithdrawUserList(cohort: cohort)
+    }
+}
