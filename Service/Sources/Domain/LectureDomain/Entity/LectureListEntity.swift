5a900a6329e5876a21d1876af40bb88f8eb090c6
shwaaaa <shwaa1026@naver.com>
2024-03-14 14:03:12 +0900

    ♻️  :: [227] LectureListEntity 수정

--- Service/Sources/Domain/LectureDomain/Entity/LectureListEntity.swift
+++ Service/Sources/Domain/LectureDomain/Entity/LectureListEntity.swift
@@ -4,9 +4,12 @@ public struct LectureListEntity: Equatable {
     public var id: String
     public let name: String
     public let content: String
+    public let semester: SemesterType
+    public let division: DivisionType
+    public let department: String
+    public let line: String
     public let startDate: String
     public let endDate: String
-    public let completeDate: String
     public let lectureType: LectureType
     public let lectureStatus: LectureStatusType
     public let headCount: Int
@@ -17,9 +20,12 @@ public struct LectureListEntity: Equatable {
         id: String,
         name: String,
         content: String,
+        semester: SemesterType,
+        division: DivisionType,
+        department: String,
+        line: String,
         startDate: String,
         endDate: String,
-        completeDate: String,
         lectureType: LectureType,
         lectureStatus: LectureStatusType,
         headCount: Int,
@@ -29,9 +35,12 @@ public struct LectureListEntity: Equatable {
         self.id = id
         self.name = name
         self.content = content
+        self.semester = semester
+        self.division = division
+        self.department = department
+        self.line = line
         self.startDate = startDate
         self.endDate = endDate
-        self.completeDate = completeDate
         self.lectureType = lectureType
         self.lectureStatus = lectureStatus
         self.headCount = headCount
