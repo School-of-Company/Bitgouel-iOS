e83606eeae130ac4f2ff92193f4cdacf58367662
shwaaaa <shwaa1026@naver.com>
2024-03-14 09:26:12 +0900

    ✨ :: [227] SemesterType 추가

---
+++ Service/Sources/Base/Enum/SemesterType.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+public enum SemesterType: String, Decodable {
+    case fistYearFallSemester = "FIRST_YEAR_FALL_SEMESTER"
+    case secondYearSpringSemester = "SECOND_YEAR_SPRING_SEMESTER"
+    case secondYearFallSemester = "SECOND_YEAR_FALL_SEMESTER"
+    case thirdYearSpringSemester = "THIRD_YEAR_SPRING_SEMESTER"
+}
+
+public extension SemesterType {
+    func display() -> String {
+        switch self {
+        case .fistYearFallSemester: return "1학년 2학기"
+        case .secondYearSpringSemester: return "2학년 1학기"
+        case .secondYearFallSemester: return "2학년 2학기"
+        case .thirdYearSpringSemester: return "3학년 1학기"
+        }
+    }
+}
