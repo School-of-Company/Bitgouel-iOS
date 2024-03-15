faec178046eed3ae864acae891758021ce0e1476
uuuunseo <jdbstjoo@gmail.com>
2024-03-15 14:07:22 +0900

    :recycle: :: [#227] LectureDomain / RepositoryImpl

--- Service/Sources/Data/Repository/Lecture/LectureRepositoryImpl.swift
+++ Service/Sources/Data/Repository/Lecture/LectureRepositoryImpl.swift
@@ -6,24 +6,36 @@ public struct LectureRepositoryImpl: LectureRepository {
     public init(remoteLectureDataSource: RemoteLectureDataSource) {
         self.remoteLectureDataSource = remoteLectureDataSource
     }
-
-    public func lectureOpen(req: LectureOpenRequestDTO) async throws {
-        try await remoteLectureDataSource.lectureOpen(req: req)
-    }
-
-    public func queryLectureList() async throws -> [LectureListEntity] {
-        try await remoteLectureDataSource.queryLectureList()
-    }
-
-    public func queryLectureDetail(userID: String) async throws -> LectureDetailEntity {
-        try await remoteLectureDataSource.queryLectureDetail(userID: userID)
-    }
-
-    public func lectureApply(userID: String) async throws {
-        try await remoteLectureDataSource.lectureApply(userID: userID)
+    
+    public func openLecture(req: OpenLectureRequestDTO) async throws {
+        try await remoteLectureDataSource.openLecture(req: req)
     }
     
-    public func lectureCancel(userID: String) async throws {
-        try await remoteLectureDataSource.lectureCancel(userID: userID)
+    public func fetchLectureList(type: String) async throws -> [LectureListEntity] {
+        try await remoteLectureDataSource.fetchLectureList(type: type)
+    }
+    
+    public func fetchLectureDetail(lectureID: String) async throws -> LectureDetailEntity {
+        try await remoteLectureDataSource.fetchLectureDetail(lectureID: lectureID)
+    }
+    
+    public func applyLecture(lectureID: String) async throws {
+        try await remoteLectureDataSource.applyLecture(lectureID: lectureID)
+    }
+    
+    public func cancelLecture(lectureID: String) async throws {
+        try await remoteLectureDataSource.cancelLecture(lectureID: lectureID)
+    }
+    
+    public func fetchInstructorList(keyword: String) async throws -> [InstructorInfoEntity] {
+        try await remoteLectureDataSource.fetchInstructorList(keyword: keyword)
+    }
+    
+    public func fetchDivisionList(keyword: String, division: String) async throws -> DivisionListEntity {
+        try await remoteLectureDataSource.fetchDivisionList(keyword: keyword, division: division)
+    }
+    
+    public func fetchDepartmentList(keyword: String) async throws -> DepartmentListEntity {
+        try await remoteLectureDataSource.fetchDepartmentList(keyword: keyword)
     }
 }
