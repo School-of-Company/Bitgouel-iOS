e1bc9009289f5f41d2eb47dca0d23c5e4c7be51c
uuuunseo <jdbstjoo@gmail.com>
2024-03-15 13:47:09 +0900

    :recycle: :: [#227] LectureDomain / lectureAPI 수정

--- Service/Sources/Domain/LectureDomain/Repository/LectureRepository.swift
+++ Service/Sources/Domain/LectureDomain/Repository/LectureRepository.swift
@@ -1,12 +1,12 @@
 import Foundation
 
 public protocol LectureRepository {
-    func onpenLecture(req: LectureOpenRequestDTO) async throws
-    func fetchLectureList() async throws -> [LectureListEntity]
+    func openLecture(req: LectureOpenRequestDTO) async throws
+    func fetchLectureList(type: String) async throws -> [LectureListEntity]
     func fetchLectureDetail(lectureID: String) async throws -> LectureDetailEntity
     func applyLecture(lectureID: String) async throws
     func cancelLecture(lectureID: String) async throws
     func fetchInstructorList(keyword: String) async throws -> InstructorInfoEntity
-    func fetchDivisionList(keyword: String, division: DivisionType) -> DivisionListEntity
-    func fetchDepartmentList(keyword: String) -> DepartmentListEntity
+    func fetchDivisionList(keyword: String, division: String) async throws -> DivisionListEntity
+    func fetchDepartmentList(keyword: String) async throws -> DepartmentListEntity
 }
