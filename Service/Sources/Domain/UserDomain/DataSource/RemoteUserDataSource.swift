079ba4d94447474001e68791e5a9d455acf9894d
uuuunseo <jdbstjoo@gmail.com>
2024-03-16 20:24:14 +0900

    :recycle: [#237] UserDomain / Query -> Fetch

--- Service/Sources/Domain/UserDomain/DataSource/RemoteUserDataSource.swift
+++ Service/Sources/Domain/UserDomain/DataSource/RemoteUserDataSource.swift
@@ -2,5 +2,5 @@ import Foundation
 
 public protocol RemoteUserDataSource: BaseRemoteDataSource<UserAPI> {
     func changePassword(req: ChangePasswordRequestDTO) async throws
-    func queryMyInfo() async throws -> MyInfoEntity
+    func fetchMyInfo() async throws -> MyInfoEntity
 }
--- Service/Sources/Domain/UserDomain/UseCase/FetchMyInfoUseCase.swift
+++ Service/Sources/Domain/UserDomain/UseCase/FetchMyInfoUseCase.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-public protocol QueryMyInfoUseCase {
+public protocol FetchMyInfoUseCase {
     func callAsFunction() async throws -> MyInfoEntity
 }
