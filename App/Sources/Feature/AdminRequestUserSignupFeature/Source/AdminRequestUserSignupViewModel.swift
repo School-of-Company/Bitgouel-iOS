c685af004041a0f50a0aac9568bca1204c831109
shwaaaa <shwaa1026@naver.com>
2024-03-19 20:07:52 +0900

    ♻️  :: [#199] AdminRequestUserSignupFeature / isisAppeove -> isApprove

--- App/Sources/Feature/AdminRequestUserSignupFeature/Source/AdminRequestUserSignupViewModel.swift
+++ App/Sources/Feature/AdminRequestUserSignupFeature/Source/AdminRequestUserSignupViewModel.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 final class AdminRequestUserSignupViewModel: BaseViewModel {
-    @Published var isAppeove: Bool = false
+    @Published var isApprove: Bool = false
     @Published var isReject: Bool = false
     @Published var isSelectedUserList = false
     @Published var isNavigateUserListDidTap = false
