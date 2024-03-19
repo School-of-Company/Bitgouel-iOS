2c55ce7b7b106311dc2be4f5fc9b4bfcda4e99e7
uuuunseo <jdbstjoo@gmail.com>
2024-03-19 20:07:10 +0900

    :sparkles: :: [#237] AuthDomain / 로그아웃 기능 추가

--- Service/Sources/Domain/AuthDomain/DataSource/RemoteAuthDataSource.swift
+++ Service/Sources/Domain/AuthDomain/DataSource/RemoteAuthDataSource.swift
@@ -1,9 +1,10 @@
 import Foundation
 
 public protocol RemoteAuthDataSource {
+    var keychain: Keychain { get }
     func login(req: LoginRequestDTO) async throws -> UserLoginEntity
     func reissueToken() async throws
-    func logout() async throws
+    func logout(accessToken: String, refreshToken: String) async throws
     func withdraw() async throws
     func studentSignup(req: StudentSignupRequestDTO) async throws
     func teacherSignup(req: TeacherSignupRequestDTO) async throws
