501f43703b8e5244f0be53de96edc2c654c821f0
uuuunseo <jdbstjoo@gmail.com>
2024-03-22 16:04:55 +0900

    :sparkles: :: [#250] EmailDomain / repositoryImpl

---
+++ Service/Sources/Data/Repository/Email/EmailRepositoryImpl.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public struct EmailRepositoryImpl: EmailRepository {
+    private let remoteEmailDataSource: any RemoteEmailDataSource
+    
+    public init(remoteEmailDataSource: any RemoteEmailDataSource) {
+        self.remoteEmailDataSource = remoteEmailDataSource
+    }
+    
+    public func sendEmailCertificationLink(email: String) async throws {
+        try await remoteEmailDataSource.sendEmailCertificationLink(email: email)
+    }
+    
+    public func fetchEmailVerificationStatus(email: String) async throws -> EmailVerificationStatusEntity {
+        try await remoteEmailDataSource.fetchEmailVerificationStatus(email: email)
+    }
+}
