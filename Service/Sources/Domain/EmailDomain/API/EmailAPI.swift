3e7ef3c4d8916f808ca44181ba485dc5cf001165
uuuunseo <jdbstjoo@gmail.com>
2024-03-22 15:52:21 +0900

    :sparkles: :: [#25] EmailDomain / API

---
+++ Service/Sources/Domain/EmailDomain/API/EmailAPI.swift
@@ -0,0 +1,69 @@
+import Moya
+import Foundation
+
+enum EmailAPI {
+    case sendEmailCertificationLink(email: String)
+    case fetchEmailVerificationStatus(email: String)
+}
+
+extension EmailAPI: BitgouelAPI {
+    typealias ErrorType = ActivityDomainError
+    
+    var domain: BitgouelDomain {
+        .email
+    }
+
+    var urlPath: String {
+        switch self {
+        case .sendEmailCertificationLink,
+             .fetchEmailVerificationStatus:
+            return ""
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .sendEmailCertificationLink:
+            return .post
+        case .fetchEmailVerificationStatus:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case let .sendEmailCertificationLink(email):
+            return .requestParameters(parameters: [
+                "email" : email
+            ], encoding: URLEncoding.httpBody)
+            
+        case let .fetchEmailVerificationStatus(email):
+            return .requestParameters(parameters: [
+                "email" : email
+            ], encoding: URLEncoding.queryString)
+        }
+    }
+    
+    var jwtTokenType: JwtTokenType {
+        switch self {
+        default:
+            return .none
+        }
+    }
+
+    var errorMap: [Int : ActivityDomainError] {
+        switch self {
+        case .sendEmailCertificationLink:
+            return [
+                400: .badRequest,
+                429: .tooManyRequest,
+                500: .internalServerError
+            ]
+        case .fetchEmailVerificationStatus:
+            return [
+                400: .badRequest,
+                401: .unauthorized
+            ]
+        }
+    }
+}
