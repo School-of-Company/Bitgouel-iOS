3bc2aa9366ddb9883fe5bf27e4bf0e11a9ba5c03
uuuunseo <jdbstjoo@gmail.com>
2024-03-25 01:45:02 +0900

    :lipstick: :: [#259] LectureListDetailFeature / 강의 상세 페이지 디자인 변경 사항 적용

--- App/Sources/Feature/LectureListDetailFeature/LectureListDetailView.swift
+++ App/Sources/Feature/LectureListDetailFeature/LectureListDetailView.swift
@@ -11,57 +11,93 @@ struct LectureListDetailView: View {
         NavigationView {
             ScrollView {
                 if let lectureDetail = viewModel.lectureDetail {
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Text("# \(lectureDetail.lectureType.display())")
-                                .bitgouelFont(.caption, color: .primary(.p3))
+                    VStack(alignment: .leading, spacing: 24) {
+                        VStack(alignment: .leading, spacing: 4) {
+                            HStack {
+                                Text("# \(lectureDetail.lectureType.display())")
+                                
+                                Text("# \(lectureDetail.division.display())")
+                            }
+                            .bitgouelFont(.caption, color: .primary(.p3))
                             
-                            Spacer()
+                            HStack {
+                                Text(lectureDetail.line)
+                                
+                                Text(lectureDetail.department)
+                            }
+                            .bitgouelFont(.caption, color: .primary(.p3))
+                            
+                            Text("\(lectureDetail.name)")
+                                .bitgouelFont(.text1, color: .greyscale(.g0))
+                                .lineSpacing(5)
+                            
+                            HStack {
+                                Text(lectureDetail.semester.display())
+                                    .bitgouelFont(.caption, color: .greyscale(.g4))
+                                
+                                HStack(spacing: 4) {
+                                    Spacer()
+                                    
+                                    Text("학점")
+                                    
+                                    Text("\(lectureDetail.credit)점")
+                                    
+                                    Text("부여")
+                                }
+                                .bitgouelFont(.caption, color: .greyscale(.g4))
+                            }
+                            
+                            HStack(spacing: 4) {
+                                Text(lectureDetail.createAt.toStringCustomFormat(format: "yyyy.M.d"))
+                                
+                                Text("에 게시")
+                                
+                                Spacer()
+                                
+                                Text(lectureDetail.lecturer)
+                                
+                                Text("교수님")
+                            }
+                            .bitgouelFont(.caption, color: .greyscale(.g7))
                         }
-                        
-                        Text("\(lectureDetail.name)")
-                            .bitgouelFont(.text1, color: .greyscale(.g0))
-                            .lineSpacing(5)
-                        
-                        HStack {
-                            Text("\(lectureDetail.startDate.toStringCustomFormat(format: "yyyy.M.d")) ~ \(lectureDetail.endDate.toStringCustomFormat(format: "yyyy.M.d"))")
-                            
-                            Spacer()
-                            
-                            Text("\(lectureDetail.headCount)/\(lectureDetail.maxRegisteredUser)명")
-                        }
-                        .bitgouelFont(.text3, color: .primary(.p5))
-                        
-                        HStack(spacing: 4) {
-                            Spacer()
-                            
-                            Text("학점")
-                            
-                            Text("\(lectureDetail.credit)점")
-                            
-                            Text("부여")
-                        }
-                        .bitgouelFont(.caption, color: .greyscale(.g4))
-                        
-                        HStack(spacing: 4) {
-                            Text(lectureDetail.createAt.toStringCustomFormat(format: "yyyy.M.d"))
-                            
-                            Text("에 게시")
-                            
-                            Spacer()
-                            
-                            Text(lectureDetail.lecturer)
-                            
-                            Text("교수님")
-                        }
-                        .bitgouelFont(.caption, color: .greyscale(.g7))
                         
                         Text(lectureDetail.content)
                             .bitgouelFont(.text3)
                             .lineSpacing(10)
                             .padding(.top, 20)
+                        
+                        VStack(alignment: .leading, spacing: 16) {
+                            BitgouelText(text: "수강 신청 기간", font: .text1)
+                            
+                            VStack(alignment: .leading, spacing: 4) {
+                                Text(lectureDetail.startDate.toStringCustomFormat(format: "yyyy년 M월 d일 H시 mm분 부터"))
+                                
+                                Text(lectureDetail.startDate.toStringCustomFormat(format: "yyyy년 M월 d일 H시 mm분 까지"))
+                            }
+                            .bitgouelFont(.text3, color: .greyscale(.g4))
+                        }
+                        
+                        VStack(alignment: .leading, spacing: 16) {
+                            BitgouelText(text: "강의 수강 날짜", font: .text1)
+                            
+                            LazyVStack(alignment: .leading, spacing: 16) {
+                                ForEach(lectureDetail.lectureDates, id: \.completeDate) { lectureDate in
+                                    Text("• \(lectureDate.completeDate.toStringCustomFormat(format: "yyyy년 MM월 dd일")) \(lectureDate.startTime.toStringCustomFormat(format: "H시 m분"))~\(lectureDate.endTime.toStringCustomFormat(format: "H시m분"))")
+                                }
+                            }
+                            .bitgouelFont(.text3, color: .greyscale(.g4))
+                        }
+                        
+                        VStack(alignment: .leading, spacing: 16) {
+                            BitgouelText(text: "모집 정원", font: .text1)
+                            
+                            BitgouelText(
+                                text: "\(lectureDetail.maxRegisteredUser)명",
+                                font: .text3
+                            )
+                            .foregroundColor(.bitgouel(.greyscale(.g4)))
+                        }
                     }
-                    .padding(.top, 24)
                     .padding(.horizontal, 28)
                 }
             }
