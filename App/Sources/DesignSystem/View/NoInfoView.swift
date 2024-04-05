dac398f9b531e6712db53bd282cca89b76b2690f
uuuunseo <jdbstjoo@gmail.com>
2024-04-05 12:09:12 +0900

    :sparkles: :: [#278] DesignSystem / 데이터 없을 때 띄울 View 추가

--- App/Sources/DesignSystem/View/NoInfoView.swift
+++ App/Sources/DesignSystem/View/NoInfoView.swift
@@ -1,7 +1,12 @@
 import SwiftUI
 
-struct DataImptyView: View {
+struct NoInfoView: View {
     var body: some View {
-        
+        VStack {
+            BitgouelAsset.Icons.emptyBox.swiftUIImage
+            
+            Text("검색 결과가 없습니다")
+                .bitgouelFont(.text3, color: .greyscale(.g4))
+        }
     }
 }
