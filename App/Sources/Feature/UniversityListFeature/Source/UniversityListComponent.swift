787d638e5e13845f794a1cc5b66a1b8e02d5aafb
uuuunseo <jdbstjoo@gmail.com>
2024-07-12 13:57:50 +0900

    :sparkles: :: [#361] UniversityListFeature / 다른 목록 관리 페이지 이동 Action 추가

--- App/Sources/Feature/UniversityListFeature/Source/UniversityListComponent.swift
+++ App/Sources/Feature/UniversityListFeature/Source/UniversityListComponent.swift
@@ -5,6 +5,6 @@ public protocol UniversityListDependency: Dependency {}
 
 public final class UniversityListComponent: Component<UniversityListDependency>, UniversityListFactory {
     public func makeView() -> some View {
-        UniversityListView()
+        UniversityListView(viewModel: .init())
     }
 }
