632034361236f2aafb8e19686a1ab0c9a503bb22
uuuunseo <jdbstjoo@gmail.com>
2024-07-15 17:15:17 +0900

    :sparkles: :: [#366] AppComponent+school

--- Service/Sources/Base/Enum/FieldType.swift
+++ Service/Sources/Base/Enum/FieldType.swift
@@ -1,8 +1,21 @@
-//
-//  FieldType.swift
-//  Service
-//
-//  Created by 정윤서 on 7/15/24.
-//
-
 import Foundation
+
+public enum FieldType: String, Codable {
+    case futuristicTransportationEquipment = "FUTURISTIC_TRANSPORTATION_EQUIPMENT"
+    case energy = "ENERGY"
+    case medicalHealthcare = "MEDICAL_HEALTHCARE"
+    case aiConvergence = "AI_CONVERGENCE"
+    case culture = "CULTURE"
+}
+
+public extension FieldType {
+    func display() -> String {
+        switch self {
+        case .futuristicTransportationEquipment: return "미래형 운송기기"
+        case .energy: return "에너지 산업"
+        case .medicalHealthcare: return "의료 헬스케어"
+        case .aiConvergence: return "AI 융복합"
+        case .culture: return "문화산업"
+        }
+    }
+}
--- Service/Sources/Base/Enum/LineType.swift
+++ Service/Sources/Base/Enum/LineType.swift
@@ -1,8 +1,19 @@
-//
-//  LineType.swift
-//  Service
-//
-//  Created by 정윤서 on 7/15/24.
-//
-
 import Foundation
+
+public enum LineType: String, Codable {
+    case industry = "INDUSTRY"
+    case commerce = "COMMERCE"
+    case agriculturalLifeHealthCare = "AGRICULTURAL_LIFE_HEALTH_CARE"
+    case customizedIndustrialDemand = "CUSTOMIZED_INDUSTRIAL_DEMAND"
+}
+
+public extension LineType {
+    func display() -> String {
+        switch self {
+        case .industry: return "공업계열"
+        case .commerce: return "상업계열"
+        case .agriculturalLifeHealthCare: return "농업생명/보건의료계열"
+        case .customizedIndustrialDemand: return "산업수요 맞춤형계열"
+        }
+    }
+}
