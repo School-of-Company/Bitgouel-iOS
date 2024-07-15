2ff3dc4de23a2a6f97778a33b360849f2d291921
uuuunseo <jdbstjoo@gmail.com>
2024-07-15 17:08:46 +0900

    :sparkles: :: [#366] SchoolDomain / Error

--- Service/Sources/Domain/SchoolDoamin/Error/SchoolDomainError.swift
+++ Service/Sources/Domain/SchoolDoamin/Error/SchoolDomainError.swift
@@ -1,14 +1,17 @@
 import Foundation
 
 public enum SchoolDomainError: Error {
-    case internalServerError
+    case badRequest
+    case notFound
+    case conflict
 }
 
 extension SchoolDomainError: LocalizedError {
     public var errorDescription: String? {
         switch self {
-        case .internalServerError:
-            return "서버 에러입니다. 지속될 시 문의해 주세요."
+        case .badRequest: return "유효하지 않은 확장자입니다."
+        case .notFound: return "존재하지 않는 학교입니다."
+        case .conflict: return "이미 존재하는 학교입니다."
         }
     }
 }
