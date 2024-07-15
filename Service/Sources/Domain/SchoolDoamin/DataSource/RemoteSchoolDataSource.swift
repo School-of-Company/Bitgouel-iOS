30779a459c11311aa918f981a12d15db89d35108
uuuunseo <jdbstjoo@gmail.com>
2024-07-15 17:09:21 +0900

    :sparkles: :: [#366] SchoolDomain / DataSource

--- Service/Sources/Domain/SchoolDoamin/DataSource/RemoteSchoolDataSource.swift
+++ Service/Sources/Domain/SchoolDoamin/DataSource/RemoteSchoolDataSource.swift
@@ -2,4 +2,8 @@ import Foundation
 
 public protocol RemoteSchoolDataSource: BaseRemoteDataSource<SchoolAPI> {
     func fetchSchoolList() async throws -> [SchoolListEntity]
+    func fetchAllSchoolName() async throws -> [String]
+    func createdSchool(req: CreatedSchoolRequestDTO) async throws
+    func modifySchool(schoolID: String, req: ModifySchoolRequestDTO) async throws
+    func deleteSchool(schoolID: String) async throws
 }
