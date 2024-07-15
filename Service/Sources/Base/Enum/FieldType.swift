b755fe26226ca49f7b490f3df2d3b0ed698cc815
uuuunseo <jdbstjoo@gmail.com>
2024-07-15 17:04:56 +0900

    :sparkles: :: [#366] Enum / Field, Line Enum 추가

---
+++ Service/Sources/Base/Enum/FieldType.swift
@@ -0,0 +1,8 @@
+//
+//  FieldType.swift
+//  Service
+//
+//  Created by 정윤서 on 7/15/24.
+//
+
+import Foundation
---
+++ Service/Sources/Base/Enum/LineType.swift
@@ -0,0 +1,8 @@
+//
+//  LineType.swift
+//  Service
+//
+//  Created by 정윤서 on 7/15/24.
+//
+
+import Foundation
