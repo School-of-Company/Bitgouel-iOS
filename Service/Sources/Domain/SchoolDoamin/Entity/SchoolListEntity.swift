b399ab8653322b41b972a148955564b0c7e559c8
uuuunseo <jdbstjoo@gmail.com>
2024-07-15 17:06:45 +0900

    :sparkles: :: [#366] SchoolDomain / Entity

--- Service/Sources/Domain/SchoolDoamin/Entity/SchoolListEntity.swift
+++ Service/Sources/Domain/SchoolDoamin/Entity/SchoolListEntity.swift
@@ -1,7 +1,10 @@
 import Foundation
 
 public struct SchoolListEntity: Equatable {
-    public let id: Int
+    public let schoolID: Int
     public let schoolName: String
-    public let clubs: [ClubEntity]
+    public let line: LineType
+    public let departments: [String]
+    public let logoImageURL: String
+    public let clubs: [SchoolWithClubsEntity]
 }
---
+++ Service/Sources/Domain/SchoolDoamin/Entity/SchoolWithClubsEntity.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+public struct SchoolWithClubsEntity: Equatable {
+    public let clubID: Int
+    public let clubName: String
+    public let field: FieldType
+}
