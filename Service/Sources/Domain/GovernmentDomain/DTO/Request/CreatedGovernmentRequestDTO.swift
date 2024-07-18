d3dc4794327a3e95389cc8598ee158dd814345c4
uuuunseo <jdbstjoo@gmail.com>
2024-07-19 00:43:53 +0900

    :sparkles: :: [#372] GovernmentDomain / DTO

--- Service/Sources/Domain/GovernmentDomain/DTO/Request/CreatedGovernmentRequestDTO.swift
+++ Service/Sources/Domain/GovernmentDomain/DTO/Request/CreatedGovernmentRequestDTO.swift
@@ -1,8 +1,14 @@
-//
-//  CreatedGovernmentRequestDTO.swift
-//  Service
-//
-//  Created by 정윤서 on 7/18/24.
-//
-
 import Foundation
+
+public struct CreatedGovernmentRequestDTO: Encodable {
+    public let field: FieldType
+    public let governmentName: String
+
+    public init(
+        field: FieldType,
+        governmentName: String
+    ) {
+        self.field = field
+        self.governmentName = governmentName
+    }
+}
--- Service/Sources/Domain/GovernmentDomain/DTO/Response/GovernmentListResponseDTO.swift
+++ Service/Sources/Domain/GovernmentDomain/DTO/Response/GovernmentListResponseDTO.swift
@@ -1,8 +1,27 @@
-//
-//  GovernmentListResponseDTO.swift
-//  Service
-//
-//  Created by 정윤서 on 7/18/24.
-//
-
 import Foundation
+
+public struct GovernmentListResponseDTO: Decodable {
+    public let governments: [GovernmentInfoResponseDTO]
+}
+
+public struct GovernmentInfoResponseDTO: Decodable {
+    public let governmentID: Int
+    public let field: FieldType
+    public let governmentName: String
+}
+
+extension GovernmentListResponseDTO {
+    func toDomain() -> [GovernmentInfoEntity] {
+        governments.map { $0.toDomain() }
+    }
+}
+
+extension GovernmentInfoResponseDTO {
+    func toDomain() -> GovernmentInfoEntity {
+        GovernmentInfoEntity(
+            governmentID: governmentID,
+            field: field,
+            governmentName: governmentName
+        )
+    }
+}
