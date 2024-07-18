f8532f28bafa335f9a5bb5daa04a912cf1c7fd20
uuuunseo <jdbstjoo@gmail.com>
2024-07-19 00:47:20 +0900

    :sparkles: :: [#372] governmentDomain / RepositoryImpl

--- Service/Sources/Data/Repository/Government/GovernmentRepositoryImpl.swift
+++ Service/Sources/Data/Repository/Government/GovernmentRepositoryImpl.swift
@@ -1,8 +1,21 @@
-//
-//  GovernmentRepositoryImpl.swift
-//  Service
-//
-//  Created by 정윤서 on 7/18/24.
-//
-
 import Foundation
+
+public struct GovernmentRepositoryImpl: GovernmentRepository {
+    private let remoteGovernmentDataSource: any RemoteGovernmentDataSource
+
+    public init(remoteGovernmentDataSource: any RemoteGovernmentDataSource) {
+        self.remoteGovernmentDataSource = remoteGovernmentDataSource
+    }
+
+    public func fetchGovernmentList() async throws -> [GovernmentInfoEntity] {
+        try await remoteGovernmentDataSource.fetchGovernmentList()
+    }
+
+    public func createdGovernment(req: CreatedGovernmentRequestDTO) async throws {
+        try await remoteGovernmentDataSource.createdGovernment(req: req)
+    }
+
+    public func deleteGovernment(governmentID: Int) async throws {
+        try await remoteGovernmentDataSource.deleteGovernment(governmentID: governmentID)
+    }
+}
