27086c8ff2d887c80b3e2948af279e937c1b7149
uuuunseo <jdbstjoo@gmail.com>
2024-07-19 00:45:48 +0900

    :sparkles: :: [#372] GovernmentDomain / Repository

--- Service/Sources/Domain/GovernmentDomain/Repository/GovernmentRepository.swift
+++ Service/Sources/Domain/GovernmentDomain/Repository/GovernmentRepository.swift
@@ -1,8 +1,7 @@
-//
-//  GovernmentRepository.swift
-//  Service
-//
-//  Created by 정윤서 on 7/18/24.
-//
-
 import Foundation
+
+public protocol GovernmentRepository {
+    func fetchGovernmentList() async throws -> [GovernmentInfoEntity]
+    func createdGovernment(req: CreatedGovernmentRequestDTO) async throws
+    func deleteGovernment(governmentID: Int) async throws
+}
