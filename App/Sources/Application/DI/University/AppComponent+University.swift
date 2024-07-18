822a0fa45504eff97aa20a0fda341778a4d4e2c9
uuuunseo <jdbstjoo@gmail.com>
2024-07-18 13:41:32 +0900

    :sparkles: :: [#370] UniversityDomain / AppComponent

---
+++ App/Sources/Application/DI/University/AppComponent+University.swift
@@ -0,0 +1,52 @@
+import NeedleFoundation
+import Service
+
+public extension AppComponent {
+    var remoteUniversityDataSource: any RemoteUniversityDataSource {
+        shared {
+            RemoteUniversityDataSourceImpl(keychain: keychain)
+        }
+    }
+    
+    var universityRepository: any UniversityRepository {
+        shared {
+            UniversityRepositoryImpl(remoteUniversityDataSource: remoteUniversityDataSource)
+        }
+    }
+    
+    var fetchUniversityListUseCase: any FetchUniversityListUseCase {
+        shared {
+            FetchUniversityListUseCaseImpl(universityRepository: universityRepository)
+        }
+    }
+    
+    var createdUniversityUseCase: any CreatedUniversityUseCase {
+        shared {
+            CreatedUniversityUseCaseImpl(universityRepository: universityRepository)
+        }
+    }
+    
+    var modifyUniversityUseCase: any ModifyUniversityUseCase {
+        shared {
+            ModifyUniversityUseCaseImpl(universityRepository: universityRepository)
+        }
+    }
+    
+    var deleteUniversityUseCase: any DeleteUniversityUseCase {
+        shared {
+            DeleteUniversityUseCaseImpl(universityRepository: universityRepository)
+        }
+    }
+    
+    var createdDepartmentUseCase: any CreatedDepartmentUseCase {
+        shared {
+            CreatedDepartmentUseCaseImpl(universityRepository: universityRepository)
+        }
+    }
+    
+    var deleteDepartmentUseCase: any DeleteDepartmentUseCase {
+        shared {
+            DeleteDepartmentUseCaseImpl(universityRepository: universityRepository )
+        }
+    }
+}
