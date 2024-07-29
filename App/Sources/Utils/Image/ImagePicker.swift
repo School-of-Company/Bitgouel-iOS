5538ca7d3e06ac549f10fca3b54903133a7203aa
uuuunseo <jdbstjoo@gmail.com>
2024-07-30 01:21:52 +0900

    :sparkles: :: [#391] Utils / ImagePicker 추가

--- App/Sources/Utils/Image/ImagePicker.swift
+++ App/Sources/Utils/Image/ImagePicker.swift
@@ -1,9 +1,37 @@
-//
-//  ImagePicker.swift
-//  Bitgouel
-//
-//  Created by 정윤서 on 7/28/24.
-//  Copyright © 2024 team.msg. All rights reserved.
-//
+import SwiftUI
 
-import Foundation
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var mode
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+}
+
+extension ImagePicker {
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            guard let image = info[.originalImage] as? UIImage else { return }
+            parent.image = image
+            parent.mode.wrappedValue.dismiss()
+        }
+    }
+}
+
