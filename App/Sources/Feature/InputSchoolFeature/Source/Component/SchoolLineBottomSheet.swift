a589ad55d0f46ced7e3a8661f11f07af312539f5
uuuunseo <jdbstjoo@gmail.com>
2024-07-30 17:34:19 +0900

    :lipstick: :: [#391] InputSchoolFeature / SchoolLineBottomSheet 추가

--- App/Sources/Feature/InputSchoolFeature/Source/Component/SchoolLineBottomSheet.swift
+++ App/Sources/Feature/InputSchoolFeature/Source/Component/SchoolLineBottomSheet.swift
@@ -1,9 +1,56 @@
-//
-//  SchoolLineBottomSheet.swift
-//  Bitgouel
-//
-//  Created by 정윤서 on 7/29/24.
-//  Copyright © 2024 team.msg. All rights reserved.
-//
+import SwiftUI
+import Service
 
-import Foundation
+public struct SchoolLineBottomSheet: View {
+    let selectedLine: LineType?
+    let lineList: [LineType] = LineType.allCases
+    let cancel: (Bool) -> Void
+    let selectLine: (LineType) -> Void
+
+    public var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            HStack {
+                BitgouelText(
+                    text: "계열",
+                    font: .title3
+                )
+
+                Spacer()
+
+                Button {
+                    cancel(false)
+                } label: {
+                    BitgouelAsset.Icons.cancel.swiftUIImage
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(Color.bitgouel(.greyscale(.g4)))
+                        .frame(width: 24, height: 24)
+                }
+            }
+
+            ScrollView {
+                LazyVStack(alignment: .leading, spacing: 0) {
+                    ForEach(lineList, id: \.self) { line in
+                        HStack {
+                            BitgouelText(
+                                text: line.display(),
+                                font: .text2
+                            )
+
+                            Spacer()
+
+                            BitgouelRadioButton(
+                                isSelected: Binding(
+                                    get: { line == selectedLine },
+                                    set: { _ in selectLine(line) }
+                                )
+                            )
+                        }
+                        .padding(.vertical, 24)
+                    }
+                }
+            }
+        }
+        .padding(.horizontal, 28)
+    }
+}
