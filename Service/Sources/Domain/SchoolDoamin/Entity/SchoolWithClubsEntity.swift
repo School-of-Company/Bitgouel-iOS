b074add560c1c3ecf0b9875e9a76449f5b9578b8
uuuunseo <jdbstjoo@gmail.com>
2024-08-05 00:20:21 +0900

    :recycle: :: [#391] FieldType 옵셔널로 변경

--- Service/Sources/Domain/SchoolDoamin/Entity/SchoolWithClubsEntity.swift
+++ Service/Sources/Domain/SchoolDoamin/Entity/SchoolWithClubsEntity.swift
@@ -3,12 +3,12 @@ import Foundation
 public struct SchoolWithClubsEntity: Equatable {
     public let clubID: Int
     public let clubName: String
-    public let field: FieldType
+    public let field: FieldType?
 
     public init(
         clubID: Int,
         clubName: String,
-        field: FieldType
+        field: FieldType?
     ) {
         self.clubID = clubID
         self.clubName = clubName
