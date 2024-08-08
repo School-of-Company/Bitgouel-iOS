f682f9ffee9bbb175e7e37dc41e0117b0b8bc3b8
uuuunseo <jdbstjoo@gmail.com>
2024-08-08 17:11:36 +0900

    :lipstick: :: [#398] LectureApplicantListFeature / 강의 신청자 명단 리스트 디자인 변경사항 적용

--- App/Sources/Feature/LectureApplicantListFeature/Source/LectureApplicantListView.swift
+++ App/Sources/Feature/LectureApplicantListFeature/Source/LectureApplicantListView.swift
@@ -9,6 +9,8 @@ struct LectureApplicantListView: View {
 
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
+            checkAllButton()
+
             ScrollView {
                 LazyVStack(alignment: .leading, spacing: 12) {
                     ForEach(viewModel.applicantList, id: \.studentID) { student in
@@ -17,7 +19,13 @@ struct LectureApplicantListView: View {
                             state: $viewModel.state,
                             isSelected: Binding(
                                 get: { student.isComplete },
-                                set: { isComplete in } 
+                                set: { isSelected in
+                                    if isSelected {
+                                        viewModel.insertStudent(studentID: student.studentID)
+                                    } else {
+                                        viewModel.removeStudent(studentID: student.studentID)
+                                    }
+                                }
                             )
                         )
 
@@ -29,6 +37,9 @@ struct LectureApplicantListView: View {
 
             Spacer()
         }
+        .overlay(alignment: .bottom) {
+            stateChangeButton()
+        }
         .padding(.horizontal, 28)
         .onAppear {
             viewModel.onAppear()
@@ -36,10 +47,83 @@ struct LectureApplicantListView: View {
         .refreshable {
             viewModel.onAppear()
         }
-        .navigationTitle("강의 신청자 명단")
+        .navigationTitle("강의 \(viewModel.state.rawValue)")
         .bitgouelToast(
             text: viewModel.errorMessage,
             isShowing: $viewModel.isErrorOccurred
         )
+        .bitgouelAlert(
+            title: "선택한 학생의 강의 이수를 \n확정하시겠습니까?",
+            description: "한 번 이수를 확정하면 수정할 수 없습니다!",
+            isShowing: $viewModel.isShowingConfirmCompletionAlert,
+            alertActions: [
+                .init(
+                    text: "취소",
+                    style: .cancel,
+                    action: {
+                        viewModel.updateIsShowingConfirmCompletionAlert(isShowing: false)
+                    }
+                ),
+                .init(
+                    text: "확정",
+                    style: .default,
+                    action: {
+                        viewModel.setLectureCompletion {
+                            viewModel.updateIsShowingConfirmCompletionAlert(isShowing: false)
+                            viewModel.updateState(state: .general)
+                        }
+                    }
+                )
+            ]
+        )
+    }
+
+    @ViewBuilder
+    func stateChangeButton() -> some View {
+        switch viewModel.state {
+        case .general:
+            BitgouelButton(
+                text: "이수 여부 체크",
+                style: .primary
+            ) {
+                viewModel.updateState(state: .check)
+            }
+
+        case .check:
+            BitgouelButton(
+                text: "선택 학생 이수 확정",
+                style: .primary
+            ) {
+                viewModel.updateIsShowingConfirmCompletionAlert(isShowing: true)
+            }
+        }
+    }
+
+    @ViewBuilder
+    func checkAllButton() -> some View {
+        switch viewModel.state {
+        case .general:
+            EmptyView()
+
+        case .check:
+            VStack(alignment: .leading, spacing: 4) {
+                Text("전체")
+                    .bitgouelFont(.caption, color: .greyscale(.g4))
+                
+                CheckButton(
+                    isSelected: Binding(
+                        get: { viewModel.isSelectedCheckAllButton },
+                        set: { isSelected in
+                            if isSelected {
+                                viewModel.insertAllStudent()
+                            } else {
+                                viewModel.removeAllStudent()
+                            }
+                            viewModel.updateIsSelectedCheckAllButton(isSelected: isSelected)
+                        }
+                    )
+                )
+            }
+        }
     }
 }
--- App/Sources/Feature/LectureApplicantListFeature/Source/LectureApplicantListViewModel.swift
+++ App/Sources/Feature/LectureApplicantListFeature/Source/LectureApplicantListViewModel.swift
@@ -1,18 +1,19 @@
 import Foundation
 import Service
 
-enum LectureApplicantListPageState {
-    case general
-    case check
+enum LectureApplicantListPageState: String {
+    case general = "신청자 명단"
+    case check = "이수 선택"
 }
 
 final class LectureApplicantListViewModel: BaseViewModel {
     @Published var applicantList: [ApplicantInfoEntity] = []
     @Published var selectedStudentID: String = ""
-    @Published var isComplete: Bool = false
+    @Published var isShowingConfirmCompletionAlert = false
     @Published var state: LectureApplicantListPageState = .general
+    @Published var isSelectedCheckAllButton: Bool = false
+    @Published var students: Set<String> = []
     var lectureID: String = ""
-    var students: [String] = []
 
     private let fetchApplicantListUseCase: any FetchApplicantListUseCase
     private let setLectureCompletionUseCase: any SetLectureCompletionUseCase
@@ -27,13 +28,52 @@ final class LectureApplicantListViewModel: BaseViewModel {
         self.setLectureCompletionUseCase = setLectureCompletionUseCase
     }
 
-    func updateApplicantInfo(isSelected: Bool, studentID: String) {
-        isComplete = isSelected
-        selectedStudentID = studentID
+    func updateIsSelectedCheckAllButton(isSelected: Bool) {
+        isSelectedCheckAllButton = isSelected
+    }
+
+    func updateIsShowingConfirmCompletionAlert(isShowing: Bool) {
+        isShowingConfirmCompletionAlert = isShowing
     }
 
     func updateState(state: LectureApplicantListPageState) {
         self.state = state
+    }
+
+    func removeStudent(studentID: String) {
+        guard students.contains(studentID) else { return }
+        guard let applicantListIndex = applicantList.map(\.studentID).firstIndex(of: studentID) else { return }
+
+        students.remove(studentID)
+        applicantList[applicantListIndex].isComplete = false
+    }
+
+    func insertStudent(studentID: String) {
+        guard let applicantListIndex = applicantList.map(\.studentID).firstIndex(of: studentID) else { return }
+        students.insert(studentID)
+        applicantList[applicantListIndex].isComplete = true
+    }
+
+    func insertAllStudent() {
+        let incompleteStudentIDs = applicantList.indices.filter {
+            !applicantList[$0].isComplete
+        }.map {
+            applicantList[$0].studentID
+        }
+
+        incompleteStudentIDs.forEach { students.insert($0) }
+
+        applicantList.indices
+            .filter { incompleteStudentIDs.contains(applicantList[$0].studentID) }
+            .forEach { applicantList[$0].isComplete = true }
+    }
+
+    func removeAllStudent() {
+        applicantList.indices
+            .filter { students.contains(applicantList[$0].studentID) }
+            .forEach { applicantList[$0].isComplete = false }
+
+        students.removeAll()
     }
 
     @MainActor
@@ -48,13 +88,16 @@ final class LectureApplicantListViewModel: BaseViewModel {
         }
     }
 
-    func modifyApplicantWhether() {
+    @MainActor
+    func setLectureCompletion(_ success: @escaping () -> Void) {
         Task {
             do {
                 try await setLectureCompletionUseCase(
                     lectureID: lectureID,
-                    students: students
+                    students: students.joined(separator: ",")
                 )
+
+                success()
             } catch {
                 errorMessage = error.lectureDomainErrorMessage()
                 isErrorOccurred = true
